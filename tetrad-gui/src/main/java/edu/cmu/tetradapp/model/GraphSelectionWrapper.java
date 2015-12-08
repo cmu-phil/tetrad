54058da8f7aed1f35406431bafa77d76e5601e09
Joseph Ramsey <jdramsey@andrew.cmu.edu>
2015-12-08 08:13:17 -0500

    Bug fix from Eric K.

--- tetrad-gui/src/main/java/edu/cmu/tetradapp/model/GraphSelectionWrapper.java
+++ tetrad-gui/src/main/java/edu/cmu/tetradapp/model/GraphSelectionWrapper.java
@@ -651,7 +651,7 @@ public class GraphSelectionWrapper implements SessionModel, GraphSource, Knowled
 
         for (Node c : graph.getChildren(z)) {
             for (Node p : graph.getParents(c)) {
-                if (p != c) {
+                if (p != z) {
                     mb.add(p);
                 }
             }
