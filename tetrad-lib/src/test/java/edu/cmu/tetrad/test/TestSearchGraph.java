e812344f80f22da27752c7856dd688ce9f623d16
Joseph Ramsey <jdramsey@andrew.cmu.edu>
2015-12-15 08:16:56 -0500

    More work on unit tests. Test pass. Finished up through the F's.

--- tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestSearchGraph.java
+++ tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestSearchGraph.java
@@ -40,8 +40,6 @@ import static org.junit.Assert.fail;
 
 
 /**
- * Tests the functions of EndpointMatrixGraph and EdgeListGraph through the Graph interface.
- *
  * @author Joseph Ramsey
  */
 public final class TestSearchGraph {
