272252c23cd97e6e6e3bc9318ee70a7089f25a8a
Joseph Ramsey <jdramsey@andrew.cmu.edu>
2016-09-12 12:35:31 -0400

    Adding Richard's measurement noise simulation to the SEM Simulation.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pattern/Fgs.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pattern/Fgs.java
@@ -46,6 +46,7 @@ public class Fgs implements Algorithm, TakesInitialGraph, HasKnowledge {
         search.setFaithfulnessAssumed(parameters.getBoolean("faithfulnessAssumed"));
         search.setKnowledge(knowledge);
         search.setVerbose(parameters.getBoolean("verbose"));
+        search.setMaxIndegree(parameters.getInt("maxIndegree"));
 
 //        if (initial != null) {
 //            search.setInitialGraph(initial);
@@ -73,6 +74,7 @@ public class Fgs implements Algorithm, TakesInitialGraph, HasKnowledge {
     public List<String> getParameters() {
         List<String> parameters = score.getParameters();
         parameters.add("faithfulnessAssumed");
+        parameters.add("maxIndegree");
         parameters.add("verbose");
         return parameters;
     }
