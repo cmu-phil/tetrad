8dced734c117b39ead32dd1e43f90ecfebb8e836
JUEST4 <juest4@pitt.edu>
2016-10-27 15:39:46 -0400

    catch NPE.  use classloader for finding properties file

--- tetrad-lib/src/main/java/edu/cmu/tetrad/latest/LatestClient.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/latest/LatestClient.java
@@ -38,48 +38,61 @@ public class LatestClient {
 
     public boolean checkLatest(String softwareName, String version) {
 
-           LOGGER.debug("running version: " + version);
+        LOGGER.debug("running version: " + version);
 
-           String BASE_URL;
-           final Properties applicationProperties =  new Properties();
-           try (final InputStream stream =
-                      this.getClass().getResourceAsStream("/tetrad-lib.properties")) {
-               applicationProperties.load(stream);
-               BASE_URL = applicationProperties.getProperty("latest.version.url");
-           } catch (Exception e) {
-               LOGGER.error("Could not read tetrad-lib.properties file", e);
-               latestResult = String.format("Running version %s but unable to contact version server.", version);
-               // return without checking for latest version
-               return false;
-           }
+        String BASE_URL = "";
+        final Properties applicationProperties = new Properties();
+        try (final InputStream stream =
+                     this.getClass().getClassLoader().getResourceAsStream("tetrad-lib.properties")) {
+            if (stream != null) {
+                applicationProperties.load(stream);
+                BASE_URL = applicationProperties.getProperty("latest.version.url");
+            } else {
+                LOGGER.warn("Could not find tetrad-lib.properties file");
+                latestResult = String.format("Running version %s but unable to contact version server.", version);
+                return false;
+            }
+        } catch (Exception e) {
+            LOGGER.error("Could not read tetrad-lib.properties file", e);
+            latestResult = String.format("Running version %s but unable to contact version server.", version);
+            // return without checking for latest version
+            return false;
+        }
 
-           try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
-               HttpGet request = new HttpGet(BASE_URL + "/latest/version/latest?softwareName=" + softwareName + "&softwareVersion=" + version);
-               request.addHeader("content-type", "application/json");
-               HttpResponse result = httpClient.execute(request);
-               String json = EntityUtils.toString(result.getEntity(), "UTF-8");
-               LOGGER.debug("response from latest version server: " + json);
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpGet request = new HttpGet(BASE_URL + "/latest/version/latest?softwareName=" + softwareName + "&softwareVersion=" + version);
+            request.addHeader("content-type", "application/json");
+            HttpResponse result = httpClient.execute(request);
+
+            if (result != null) {
+                String json = EntityUtils.toString(result.getEntity(), "UTF-8");
+                LOGGER.debug("response from latest version server: " + json);
+
+                if (json != null) {
+                    com.google.gson.Gson gson = new com.google.gson.Gson();
+                    SoftwareVersion softwareVersion = gson.fromJson(json, SoftwareVersion.class);
+
+                    if (softwareVersion.getSoftwareVersion().equalsIgnoreCase(version)) {
+                        latestResult = String.format("Running version %s which is the latest version.  To disable checking use the skip-latest option.", version);
+                        return true;
+                    } else {
+                        latestResult = String.format("Running version %s but the latest version is %s.  To disable checking use the skip-latest option.", version, softwareVersion.getSoftwareVersion());
+                        return false;
+                    }
+                }
+            }
+
+            latestResult = String.format("Unable to communicate with version server. Running version %s.  To disable checking use the skip-latest option.", version);
+            return false;
 
 
-               com.google.gson.Gson gson = new com.google.gson.Gson();
-               SoftwareVersion softwareVersion = gson.fromJson(json, SoftwareVersion.class);
+        } catch (Exception ex) {
 
-               if (softwareVersion.getSoftwareVersion().equalsIgnoreCase(version)) {
-                   latestResult = String.format("Running version %s which is the latest version.  To disable checking use the skip-latest option.", version);
-                   return true;
-               } else {
-                   latestResult = String.format("Running version %s but the latest version is %s.  To disable checking use the skip-latest option.", version, softwareVersion.getSoftwareVersion());
-                   return false;
-               }
+            LOGGER.error("Could not contact server for latest version", ex);
+            latestResult = String.format("Running version %s but unable to contact latest version server.  To disable checking use the skip-latest option.", version);
+            return false;
 
+        }
 
-           } catch (Exception ex) {
-
-               LOGGER.error("Could not contact server for latest version", ex);
-               latestResult = String.format("Running version %s but unable to contact latest version server.  To disable checking use the skip-latest option.", version);
-               return false;
-
-           }
-
-       }
+    }
 }
