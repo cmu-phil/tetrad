25f8a357da27ca6f5257ebef771f50dd90dd5b22
ruben sanchez-romero <rubensanchez-romero@CMU-806414.WV.CC.CMU.EDU>
2016-11-18 15:15:06 -0500

    Corrections on ArrowConfusion

--- tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/statistic/utils/ArrowConfusion.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/statistic/utils/ArrowConfusion.java
@@ -13,7 +13,7 @@ import java.util.Set;
  * A true positive arrow is counted for X*->Y in the estimated graph if X is not adjacent
  * to Y or X--Y or X<--Y.
  *
- * @author jdramsey
+ * @author jdramsey, rubens (November, 2016)
  */
 public class ArrowConfusion {
 
@@ -33,21 +33,27 @@ public class ArrowConfusion {
         arrowsTp = 0;
         arrowsFp = 0;
         arrowsFn = 0;
-        TCtp = 0; //two-cycle
+        TCtp = 0; //for the two-cycle accuracy
         TCfn = 0;
         TCfp = 0;
 
-        Set<Edge> allOriented = new HashSet<>();
-        allOriented.addAll(this.truth.getEdges());
-        allOriented.addAll(this.est.getEdges());
-
-        System.out.println(allOriented);
-
-        for (Edge edge : allOriented) {
 
 
-            Edge edge1 = this.est.getDirectedEdge(edge.getNode1(), edge.getNode2());
-            System.out.println(edge1 + "(est)");
+        // Get edges from the true Graph to compute TruePositives, TrueNegatives and FalseNeagtives
+        //    System.out.println(this.truth.getEdges());
+
+        for (Edge edge : this.truth.getEdges()) {
+
+            List<Edge> edges1 = this.est.getEdges(edge.getNode1(), edge.getNode2());
+            Edge edge1;
+
+            if (edges1.size() == 1) {
+                edge1 = edges1.get(0);
+            } else {
+                edge1 = this.est.getDirectedEdge(edge.getNode1(), edge.getNode2());
+            }
+
+            //      System.out.println(edge1 + "(est)");
 
             Endpoint e1Est = null;
             Endpoint e2Est = null;
@@ -56,11 +62,19 @@ public class ArrowConfusion {
                 e1Est = edge1.getProximalEndpoint(edge.getNode1());
                 e2Est = edge1.getProximalEndpoint(edge.getNode2());
             }
-            System.out.println(e1Est);
-            System.out.println(e2Est);
+            //      System.out.println(e1Est);
+            //      System.out.println(e2Est);
 
-            Edge edge2 = this.truth.getDirectedEdge(edge.getNode1(), edge.getNode2());
-            System.out.println(edge2 + "(truth)");
+            List<Edge> edges2 = this.truth.getEdges(edge.getNode1(), edge.getNode2());
+            Edge edge2;
+
+            if (edges2.size() == 1) {
+                edge2 = edges2.get(0);
+            } else {
+                edge2 = this.truth.getDirectedEdge(edge.getNode1(), edge.getNode2());
+            }
+
+            //       System.out.println(edge2 + "(truth)");
 
             Endpoint e1True = null;
             Endpoint e2True = null;
@@ -69,17 +83,9 @@ public class ArrowConfusion {
                 e1True = edge2.getProximalEndpoint(edge.getNode1());
                 e2True = edge2.getProximalEndpoint(edge.getNode2());
             }
-            System.out.println(e1True);
-            System.out.println(e2True);
+            //       System.out.println(e1True);
+            //       System.out.println(e2True);
 
-
-            if (e1Est == Endpoint.ARROW && e1True != Endpoint.ARROW) {
-                arrowsFp++;
-            }
-
-            if (e2Est == Endpoint.ARROW && e2True != Endpoint.ARROW) {
-                arrowsFp++;
-            }
 
             if (e1True == Endpoint.ARROW && e1Est != Endpoint.ARROW) {
                 arrowsFn++;
@@ -105,36 +111,106 @@ public class ArrowConfusion {
                 arrowsTn++;
             }
 
-     // test for 2-cycle
-            List<Edge> TwoCycle1 = this.truth.getEdges(edge.getNode1(), edge.getNode2());
-            List<Edge> TwoCycle2 = this.est.getEdges(edge.getNode1(),edge.getNode2());
 
-            if(TwoCycle1.size() == 2 && TwoCycle2.size() == 2){
-                System.out.println("2-cycle correctly inferred " + TwoCycle1);
-                TCtp++; }
+        }
+// Get edges from the estimated graph to compute only FalsePositives
+        // System.out.println(this.est.getEdges());
 
-            if(TwoCycle1.size() == 2 && TwoCycle2.size() != 2){
-                System.out.println("2-cycle not inferred " + TwoCycle1);
-                TCfn++; }
+        for (Edge edge : this.est.getEdges()) {
 
-            if(TwoCycle1.size() != 2 && TwoCycle2.size() == 2){
-                System.out.println("2-cycle falsely inferred" + TwoCycle2);
-                TCfp++;}
+            List<Edge> edges1 = this.est.getEdges(edge.getNode1(), edge.getNode2());
+            Edge edge1;
+
+            if (edges1.size() == 1) {
+                edge1 = edges1.get(0);
+            } else {
+                edge1 = this.est.getDirectedEdge(edge.getNode1(), edge.getNode2());
+            }
+            //      System.out.println(edge1 + "(est)");
+
+            Endpoint e1Est = null;
+            Endpoint e2Est = null;
+
+            if (edge1 != null) {
+                e1Est = edge1.getProximalEndpoint(edge.getNode1());
+                e2Est = edge1.getProximalEndpoint(edge.getNode2());
+            }
+            //       System.out.println(e1Est);
+            //       System.out.println(e2Est);
+
+            List<Edge> edges2 = this.truth.getEdges(edge.getNode1(), edge.getNode2());
+            Edge edge2;
+
+            if (edges2.size() == 1) {
+                edge2 = edges2.get(0);
+            } else {
+                edge2 = this.truth.getDirectedEdge(edge.getNode1(), edge.getNode2());
+            }
+
+            //          System.out.println(edge2 + "(truth)");
+
+            Endpoint e1True = null;
+            Endpoint e2True = null;
+
+            if (edge2 != null) {
+                e1True = edge2.getProximalEndpoint(edge.getNode1());
+                e2True = edge2.getProximalEndpoint(edge.getNode2());
+            }
+            //          System.out.println(e1True);
+            //          System.out.println(e2True);
+
+
+            if (e1Est == Endpoint.ARROW && e1True != Endpoint.ARROW) {
+                arrowsFp++;
+            }
+
+            if (e2Est == Endpoint.ARROW && e2True != Endpoint.ARROW) {
+                arrowsFp++;
             }
 
 
-        System.out.println(arrowsTp);
-        System.out.println(arrowsFp);
-        System.out.println(arrowsFn);
-        System.out.println(arrowsTn);
+        }
 
+
+        // test for 2-cycle
+        Set<Edge> allOriented = new HashSet<>();
+        allOriented.addAll(this.truth.getEdges());
+        allOriented.addAll(this.est.getEdges());
+
+        for (Edge edge : allOriented) {
+
+            List<Edge> TwoCycle1 = this.truth.getEdges(edge.getNode1(), edge.getNode2());
+            List<Edge> TwoCycle2 = this.est.getEdges(edge.getNode1(), edge.getNode2());
+
+            if (TwoCycle1.size() == 2 && TwoCycle2.size() == 2) {
+                System.out.println("2-cycle correctly inferred " + TwoCycle1);
+                TCtp++;
+            }
+
+            if (TwoCycle1.size() == 2 && TwoCycle2.size() != 2) {
+                System.out.println("2-cycle not inferred " + TwoCycle1);
+                TCfn++;
+            }
+
+            if (TwoCycle1.size() != 2 && TwoCycle2.size() == 2) {
+                System.out.println("2-cycle falsely inferred" + TwoCycle2);
+                TCfp++;
+            }
+        }
+
+  /*      System.out.println(arrowsTp);
+        System.out.println(arrowsTn);
+        System.out.println(arrowsFn);
+        System.out.println(arrowsFp);
+*/
         //divide by 2, the 2cycle accuracy is duplicated due to how getEdges is used
         TCtp = TCtp / 2;
         TCfn = TCfn / 2;
         TCfp = TCfp / 2;
-        System.out.println(TCtp);
+  /*      System.out.println(TCtp);
         System.out.println(TCfn);
         System.out.println(TCfp);
+        */
     }
 
 
@@ -154,15 +230,17 @@ public class ArrowConfusion {
         return arrowsTn;
     }
 
-    public int getTwoCycleTp() { return TCtp; }
+    public int getTwoCycleTp() {
+        return TCtp;
+    }
 
-    public int getTwoCycleFp() { return TCfp; }
+    public int getTwoCycleFp() {
+        return TCfp;
+    }
 
-    public int getTwoCycleFn() { return TCfn; }
-
-
-
-
+    public int getTwoCycleFn() {
+        return TCfn;
+    }
 
 
 }
