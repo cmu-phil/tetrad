e2cf858cd5941b79fbb480b6c041db8aa25c4287
Joseph Ramsey <jdramsey@andrew.cmu.edu>
2017-03-05 20:31:21 -0500

    Work on FANG. This one works well.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/search/Fang.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/Fang.java
@@ -31,6 +31,7 @@ import java.util.List;
 import static edu.cmu.tetrad.util.StatUtils.covariance;
 import static java.lang.Math.abs;
 import static java.lang.Math.signum;
+import static java.lang.Math.sqrt;
 
 /**
  * Fast adjacency search followed by robust skew orientation. Checks are done for adding
@@ -108,67 +109,35 @@ public final class Fang implements GraphSearch {
 
         Graph graph = new EdgeListGraph(variables);
 
+        int n = colData[0].length;
+
         for (int i = 0; i < variables.size(); i++) {
             for (int j = i + 1; j < variables.size(); j++) {
                 Node X = variables.get(i);
                 Node Y = variables.get(j);
 
-                int n = colData[i].length;
+                final double[] x = colData[i];
+                final double[] y = colData[j];
 
-                final double[] x = new double[n];
-                final double[] y = new double[n];
+                double c1 = s(x, y, 1, 0);
+                double c2 = s(x, y, 0, 1);
+                double v1 = s(x, x, 1, 0);
+                double v2 = s(y, y, 0, 1);
 
-                for (int k = 0; k < n; k++) {
-                    x[k] = colData[i][k];
-                    y[k] = colData[j][k];
-                }
-
-                double sxyxp = 0.0;
-                double sxyyp = 0.0;
-                double sxxxp = 0.0;
-                double syyyp = 0.0;
-
-                int na = 0;
-                int nb = 0;
-
-                for (int k = 0; k < n; k++) {
-                    if (x[k] > 0) {
-                        sxyxp += x[k] * y[k];
-                        sxxxp += x[k] * x[k];
-                        na++;
-                    }
-
-                    if (y[k] > 0) {
-                        sxyyp += x[k] * y[k];
-                        syyyp += y[k] * y[k];
-                        nb++;
-                    }
-                }
-
-                sxyxp /= na;
-                sxyyp /= nb;
-                sxxxp /= na;
-                syyyp /= nb;
-
-                double q1 = sxyxp / sxxxp;
-                double q2 = sxyyp / syyyp;
-
+                double q1 = c1 / v1;
+                double q2 = c2 / v2;
 
                 if (G0.isAdjacentTo(X, Y) || ((q1 < 0.2 || q2 < 0.2) && abs(q1 - q2) > 0.2)) {
-                    double c = covarianceOfPart(x, y, 0, 0);
-                    double c1 = covarianceOfPart(x, y, 1, 0);
-                    double c2 = covarianceOfPart(x, y, 0, 1);
-                    double c3 = covarianceOfPart(x, y, -1, 0);
-                    double c4 = covarianceOfPart(x, y, 0, -1);
-
-                    double R = c * (sxyxp - sxyyp);
+                    double c = s(x, y, 0, 0);
+                    double c3 = s(x, y, -1, 0);
+                    double c4 = s(x, y, 0, -1);
+                    double R = c * (c1 - c2);
 
                     if (knowledgeOrients(X, Y)) {
                         graph.addDirectedEdge(X, Y);
                     } else if (knowledgeOrients(Y, X)) {
                         graph.addDirectedEdge(Y, X);
-                    } else if (!(signum(c) == signum(c1) && signum(c) == signum(c3))
-                            && !(signum(c) == signum(c2) && signum(c) == signum(c4))) {
+                    } else if (signum(c1) != signum(c3) && signum(c2) != signum(c4)) {
                         Edge edge1 = Edges.directedEdge(X, Y);
                         Edge edge2 = Edges.directedEdge(Y, X);
 
@@ -203,55 +172,48 @@ public final class Fang implements GraphSearch {
         return graph;
     }
 
-    private double covarianceOfPart(double[] xData, double[] yData, int q, int s) {
-        final int n = xData.length;
+    private double s(double[] x, double[] y, int q, int s) {
+        double sxy = 0.0;
+        double sxx = 0.0;
+        double syy = 0.0;
+        int n = 0;
 
-        double[] x = new double[n];
-        double[] y = new double[n];
-
-        for (int i = 0; i < n; i++) {
+        for (int k = 0; k < x.length; k++) {
             if (q == 0 && s == 0) {
-                x[i] = xData[i];
-                y[i] = yData[i];
-            } else if (q == 1 && s == 0 && xData[i] > 0) {
-                x[i] = xData[i];
-                y[i] = yData[i];
-            } else if (q == 0 && s == 1 && yData[i] > 0) {
-                x[i] = xData[i];
-                y[i] = yData[i];
-            } else if (q == -1 && s == 0 && xData[i] < 0) {
-                x[i] = xData[i];
-                y[i] = yData[i];
-            } else if (q == 0 && s == -1 && yData[i] < 0) {
-                x[i] = xData[i];
-                y[i] = yData[i];
+                sxy += x[k] * y[k];
+                sxx += x[k] * x[k];
+                syy += y[k] * y[k];
+                n++;
+            } else if (q == 1 && s == 0 && x[k] > 0) {
+                sxy += x[k] * y[k];
+                sxx += x[k] * x[k];
+                syy += y[k] * y[k];
+                n++;
+            } else if (q == 0 && s == 1 && y[k] > 0) {
+                sxy += x[k] * y[k];
+                sxx += x[k] * x[k];
+                syy += y[k] * y[k];
+                n++;
+            } else if (q == -1 && s == 0 && x[k] < 0) {
+                sxy += x[k] * y[k];
+                sxx += x[k] * x[k];
+                syy += y[k] * y[k];
+                n++;
+            } else if (q == 0 && s == -1 && y[k] < 0) {
+                sxy += x[k] * y[k];
+                sxx += x[k] * x[k];
+                syy += y[k] * y[k];
+                n++;
             }
         }
 
-        x = nonzero(x);
-        y = nonzero(y);
-
-        return covariance(x, y);
-    }
-
-    private double[] nonzero(double[] q3) {
-        int N = 0;
-
-        for (double aQ3 : q3) {
-            if (aQ3 != 0) N++;
+        for (int k = 0; k < n; k++) {
+            sxy += x[k] * y[k] / (double) n;
+            sxx += x[k] * x[k] / (double) n;;
+            syy += y[k] * y[k] / (double) n;;
         }
 
-        double[] ret = new double[N];
-
-        int t = 0;
-
-        for (double aQ3 : q3) {
-            if (aQ3 != 0) {
-                ret[t++] = aQ3;
-            }
-        }
-
-        return ret;
+        return sxy / sqrt(sxx * syy);
     }
 
     /**
