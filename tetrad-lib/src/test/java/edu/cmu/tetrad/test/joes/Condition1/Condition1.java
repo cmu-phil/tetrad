b74ff3965b56785530c0b9081ac04e1d0fac695f
jdramsey <jdramsey@andrew.cmu.edu>
2017-07-08 13:52:38 -0400

    Moved Condition1.java into Joe's test package.

--- tetrad-lib/src/test/java/edu/cmu/tetrad/test/joes/Condition1/Condition1.java
+++ tetrad-lib/src/test/java/edu/cmu/tetrad/test/joes/Condition1/Condition1.java
@@ -19,7 +19,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA //
 ///////////////////////////////////////////////////////////////////////////////
 
-package Condition1;
+package edu.cmu.tetrad.test.joes.Condition1;
 
 import edu.cmu.tetrad.algcomparison.Comparison;
 import edu.cmu.tetrad.algcomparison.algorithm.Algorithms;
