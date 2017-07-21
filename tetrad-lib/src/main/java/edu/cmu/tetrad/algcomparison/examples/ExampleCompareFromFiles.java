467b3841b5c5ac433bb31b714eea2f54ee805693
chirayukong <chirayu.kong@gmail.com>
2017-07-21 17:55:53 -0400

    Revert "Merge remote-tracking branch 'origin/development' into development"
    This reverts commit 30cc4eaa0ec798b75d3ec391344ce81fff8d00fc, reversing
    changes made to d24d8f643359f0d21b70c42975846ff303f69182.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/examples/ExampleCompareFromFiles.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/examples/ExampleCompareFromFiles.java
@@ -1,21 +1,29 @@
-/*
-* // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA //
-* ///////////////////////////////////////////////////////////////////////////////
-* <p/>
-* package edu.cmu.tetrad.algcomparison.examples;
-* <p/> */
+///////////////////////////////////////////////////////////////////////////////
+// For information as to what this class does, see the Javadoc, below.       //
+// Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,       //
+// 2007, 2008, 2009, 2010, 2014, 2015 by Peter Spirtes, Richard Scheines, Joseph   //
+// Ramsey, and Clark Glymour.                                                //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify      //
+// it under the terms of the GNU General Public License as published by      //
+// the Free Software Foundation; either version 2 of the License, or         //
+// (at your option) any later version.                                       //
+//                                                                           //
+// This program is distributed in the hope that it will be useful,           //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+// GNU General Public License for more details.                              //
+//                                                                           //
+// You should have received a copy of the GNU General Public License         //
+// along with this program; if not, write to the Free Software               //
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA //
+///////////////////////////////////////////////////////////////////////////////
+
 package edu.cmu.tetrad.algcomparison.examples;
 
 import edu.cmu.tetrad.algcomparison.Comparison;
 import edu.cmu.tetrad.algcomparison.algorithm.Algorithms;
-import edu.cmu.tetrad.algcomparison.algorithm.oracle.pag.Fci;
-import edu.cmu.tetrad.algcomparison.algorithm.oracle.pag.Gfci;
-import edu.cmu.tetrad.algcomparison.algorithm.oracle.pag.Rfci;
 import edu.cmu.tetrad.algcomparison.algorithm.oracle.pattern.*;
-import edu.cmu.tetrad.algcomparison.independence.FisherZ;
-import edu.cmu.tetrad.algcomparison.score.BdeuScore;
-import edu.cmu.tetrad.algcomparison.score.DiscreteBicScore;
-import edu.cmu.tetrad.algcomparison.score.SemBicScore;
 import edu.cmu.tetrad.algcomparison.independence.FisherZ;
 import edu.cmu.tetrad.util.Parameters;
 import edu.cmu.tetrad.algcomparison.statistic.*;
@@ -36,13 +44,8 @@ public class ExampleCompareFromFiles {
         Parameters parameters = new Parameters();
 
         // Can leave the simulation parameters out since
-        // we're loading from file here
-        parameters.set("alpha", .01);
-        parameters.set("maxDistinctValuesDiscrete", 5);
-        parameters.set("symmetricFirstStep", true);
-        parameters.set("depth", 4);
-        parameters.set("penaltyDiscount", 1,8);
-        parameters.set("numRuns",10);
+        // we're loading from file here.
+        parameters.set("alpha", 1e-4);
 
         Statistics statistics = new Statistics();
 
@@ -52,8 +55,6 @@ public class ExampleCompareFromFiles {
         statistics.add(new AdjacencyRecall());
         statistics.add(new ArrowheadPrecision());
         statistics.add(new ArrowheadRecall());
-        statistics.add(new TailPrecision());
-        statistics.add(new TailRecall());
         statistics.add(new MathewsCorrAdj());
         statistics.add(new MathewsCorrArrow());
         statistics.add(new F1Adj());
@@ -65,37 +66,25 @@ public class ExampleCompareFromFiles {
         statistics.setWeight("AR", 0.5);
         statistics.setWeight("AHP", 1.0);
         statistics.setWeight("AHR", 0.5);
-        statistics.setWeight("TP", 1.0);
-        statistics.setWeight("TR", 0.5);
 
         Algorithms algorithms = new Algorithms();
 
-//        algorithms.add(new Gfci(new ChiSquare(), new BdeuScore()));
-        algorithms.add(new Fci(new FisherZ()));
-//        algorithms.add(new Fci(new ChiSquare()));
-//        algorithms.add(new Rfci(new ChiSquare()));
-        algorithms.add(new Rfci(new FisherZ()));
-        algorithms.add(new Gfci(new FisherZ(), new SemBicScore()));
-//        algorithms.add(new Fges(new BdeuScore(),true));
-//        algorithms.add(new Fges(new DiscreteBicScore(),true));
-        algorithms.add(new Fges(new SemBicScore()));
-//        algorithms.add(new Gfci(new ChiSquare(), new DiscreteBicScore())));
-//        algorithms.add(new Fges(new BdeuScore()));
-//        algorithms.add(new Fges(new DiscreteBicScore()));
-//        algorithms.add(new PcMax(new FisherZ(), false));
-//        algorithms.add(new PcMax(new ChiSquare(),true));
-//        algorithms.add(new PcMax(new FisherZ(), false));
-//        algorithms.add(new Pc(new FisherZ()));
+        algorithms.add(new Pc(new FisherZ()));
+        algorithms.add(new Cpc(new FisherZ()));
+        algorithms.add(new PcStable(new FisherZ()));
+        algorithms.add(new CpcStable(new FisherZ()));
 
         Comparison comparison = new Comparison();
-        comparison.setShowAlgorithmIndices(true);
+        comparison.setShowAlgorithmIndices(false);
         comparison.setShowSimulationIndices(false);
         comparison.setSortByUtility(true);
         comparison.setShowUtilities(true);
-        comparison.setParallelized(false);
-        comparison.setSaveGraphs(true);
-        //DagToPag p = new DagToPag(graph);
+        comparison.setParallelized(true);
 
-        comparison.compareFromFiles("comparison1", algorithms, statistics, parameters);
+        comparison.compareFromFiles("comparison", algorithms, statistics, parameters);
     }
 }
+
+
+
+
