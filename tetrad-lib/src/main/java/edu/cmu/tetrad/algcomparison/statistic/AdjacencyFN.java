d3daa56066a688e34fd3bfca9bb02f1b3912eae2
jdramsey <jdramsey@andrew.cmu.edu>
2017-08-03 16:00:25 -0400

    Fixed two problems for Zhou.

---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/statistic/AdjacencyFN.java
@@ -0,0 +1,35 @@
+package edu.cmu.tetrad.algcomparison.statistic;
+
+import edu.cmu.tetrad.algcomparison.statistic.utils.AdjacencyConfusion;
+import edu.cmu.tetrad.graph.Graph;
+
+/**
+ * The adjacency precision. The true positives are the number of adjacencies in both
+ * the true and estimated graphs.
+ *
+ * @author jdramsey
+ */
+public class AdjacencyFN implements Statistic {
+    static final long serialVersionUID = 23L;
+
+    @Override
+    public String getAbbreviation() {
+        return "AFN";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Adjacency False Negatives";
+    }
+
+    @Override
+    public double getValue(Graph trueGraph, Graph estGraph) {
+        AdjacencyConfusion adjConfusion = new AdjacencyConfusion(trueGraph, estGraph);
+        return adjConfusion.getAdjFn();
+    }
+
+    @Override
+    public double getNormValue(double value) {
+        return value;
+    }
+}
