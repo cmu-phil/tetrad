d6d4f8e35a2fa71092da954ec30403a217558050
jdramsey <jdramsey@andrew.cmu.edu>
2018-01-22 09:22:36 -0500

    Some adjustemnts to knowledge.

--- tetrad-lib/src/main/java/joe/test/TestParseUrl.java
+++ tetrad-lib/src/main/java/joe/test/TestParseUrl.java
@@ -2,7 +2,14 @@ package joe.test;
 
 import edu.cmu.tetrad.data.*;
 import edu.cmu.tetrad.graph.Node;
+import edu.cmu.tetrad.regression.RegressionDataset;
+import edu.cmu.tetrad.regression.RegressionResult;
+import edu.cmu.tetrad.search.TimeSeriesUtils;
+import edu.cmu.tetrad.util.DataConvertUtils;
 import edu.pitt.dbmi.data.Dataset;
+import edu.pitt.dbmi.data.Delimiter;
+import edu.pitt.dbmi.data.reader.DataReader;
+import edu.pitt.dbmi.data.reader.tabular.ContinuousTabularDataFileReader;
 import org.junit.Test;
 
 import java.io.*;
@@ -12,36 +19,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestParseUrl {
-
+    String[] coins = {
+                    "bitcoin",
+//                    "ethereum",
+//                    "ripple",
+//                    "bitcoin-cash",
+//                    "cardano",
+//                    "litecoin",
+//                    "monero",
+//                    "bitconnect",
+//                    "sirin-labs-token",
+//            "electroneum"
+    };
 
 
     @Test
     public void test1() {
-//        URL url;
         InputStream is = null;
         BufferedReader br;
         String line;
 
         try {
-            String[] coins = {
-                    "bitcoin",
-                    "ethereum",
-                    "ripple",
-                    "bitcoin-cash",
-                    "cardano",
-                    "litecoin",
-                    "monero"
-            };
 
             for (String coin : coins) {
-                URL url = new URL("https://coinmarketcap.com/currencies/" + coin + "/historical-data/?start=20170821&end=20180121");
-//                url = new URL(_url);
+                URL url = new URL("https://coinmarketcap.com/currencies/" + coin + "/historical-data/?start=20170121&end=20180121");
                 is = url.openStream();  // throws an IOException
                 br = new BufferedReader(new InputStreamReader(is));
 
                 List<double[]> recordList = new ArrayList<>();
 
                 while ((line = br.readLine()) != null) {
+
                     if (line.contains("<tr class=\"text-right\">")) {
                         for (int i = 0; i < 1; i++) {
                             br.readLine();
@@ -54,14 +62,17 @@ public class TestParseUrl {
                         for (int i = 0; i < 6; i++) {
                             line = br.readLine();
 
-                            if (line.contains("-")){
+//                            line = line.split("<")[1];
+//                            line = line.split(">")[1];
+//                            line = line.replaceAll(",", "");
+
+                            line = line.split("\"")[1];
+
+                            if (line.equalsIgnoreCase("-")) {
                                 fullRecord = false;
                                 continue;
                             }
 
-                            line = line.split("<")[1];
-                            line = line.split(">")[1];
-                            line = line.replaceAll(",", "");
                             double d = Double.parseDouble(line);
                             record[i] = d;
                         }
@@ -103,6 +114,43 @@ public class TestParseUrl {
             } catch (IOException ioe) {
                 // nothing to see here
             }
+        }
+    }
+
+    @Test
+    public void test2() {
+
+        DataReader reader = new ContinuousTabularDataFileReader(new File("/Users/user/Downloads/" + coins[0]), Delimiter.TAB);
+
+        try {
+            Dataset dataset = reader.readInData();
+            DataSet dataSet = (DataSet) DataConvertUtils.toDataModel(dataset);
+
+            System.out.println(dataSet);
+            DataSet lagged = TimeSeriesUtils.createLagData(dataSet, 10);
+
+            List<Node> regressors = new ArrayList<>(lagged.getVariables());
+
+            Node target = regressors.get(4);
+
+            for (Node node : new ArrayList<>(regressors)) {
+                final int lag = TimeSeriesUtils.getLag(node.getName());
+                final String name = TimeSeriesUtils.getNameNoLag(node.getName());
+                if (lag == 0 ) regressors.remove(node);
+                if (!name.equalsIgnoreCase("MarketCap")) regressors.remove(node);
+            }
+
+            System.out.println("Coin = " + coins[0]);
+            System.out.println("Target = " + target);
+
+            RegressionDataset regressionDataset = new RegressionDataset(lagged);
+
+            RegressionResult result = regressionDataset.regress(target, regressors);
+
+            System.out.println(result);
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
