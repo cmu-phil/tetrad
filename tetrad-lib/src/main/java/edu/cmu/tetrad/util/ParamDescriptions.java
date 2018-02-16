03c131d4baef46c11900eeffbdbbc498bf06991f
jdramsey <jdramsey@andrew.cmu.edu>
2018-02-16 07:23:04 -0500

    Set the default number of subsamples to 30. It was set to 8, too low.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/util/ParamDescriptions.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/util/ParamDescriptions.java
@@ -208,7 +208,7 @@ public class ParamDescriptions {
 
         map.put("numSubsamples", new ParamDescription(
                 "The number of subsamples",
-                8, 1, Integer.MAX_VALUE));
+                30, 1, Integer.MAX_VALUE));
 
         map.put("percentSubsampleSize", new ParamDescription(
                 "Percentage of records to include in a random subsample",
