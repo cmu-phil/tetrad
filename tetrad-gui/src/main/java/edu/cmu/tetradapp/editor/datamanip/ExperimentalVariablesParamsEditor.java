e65f529b229c97866427f16b97af9a016d0edca1
yuanzhou <yuanzhou19@gmail.com>
2018-05-24 15:38:29 -0400

    Adding button listener to add new interventional variable to all context

--- tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/datamanip/ExperimentalVariablesParamsEditor.java
+++ tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/datamanip/ExperimentalVariablesParamsEditor.java
@@ -11,18 +11,24 @@ import edu.cmu.tetrad.data.DataSet;
 import edu.cmu.tetrad.util.Parameters;
 import edu.cmu.tetradapp.editor.FinalizingParameterEditor;
 import edu.cmu.tetradapp.model.DataWrapper;
+import edu.cmu.tetradapp.util.StringTextField;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.util.LinkedList;
 import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 
 /**
  *
@@ -41,7 +47,8 @@ public class ExperimentalVariablesParamsEditor extends JPanel implements Finaliz
     
     private DataModelList dataSets = null;
 
-
+    private StringTextField interventionalVarNameField;
+    
     //==========================CONSTUCTORS===============================//
     
     /**
@@ -62,9 +69,11 @@ public class ExperimentalVariablesParamsEditor extends JPanel implements Finaliz
     public void setup() {
         System.out.println("=========ExperimentalVariablesParamsEditor setup()=========");
 
+        List<JTextPane> contextAreaList = new LinkedList<>();
+        
         // Container
         Box container = Box.createVerticalBox();
-        container.setPreferredSize(new Dimension(500, 460));
+        container.setPreferredSize(new Dimension(640, 460));
         
         final List<String> variables = this.sourceDataSet.getVariableNames();
 
@@ -80,46 +89,97 @@ public class ExperimentalVariablesParamsEditor extends JPanel implements Finaliz
         JList varNamesList = new JList(varNamesListModel);
         
         JScrollPane variablesListScroller = new JScrollPane(varNamesList);
-        variablesListScroller.setPreferredSize(new Dimension(450, 150));
-        variablesListScroller.setAlignmentX(LEFT_ALIGNMENT);
+        variablesListScroller.setPreferredSize(new Dimension(480, 120));
+        
+        String variablesContainerBorderTitle = "Variables";
+        // Use a titled border with 5 px inside padding
+        variablesListScroller.setBorder(new CompoundBorder(BorderFactory.createTitledBorder(variablesContainerBorderTitle), new EmptyBorder(5, 5, 5, 5)));
 
         //Lay out the label and scroll pane from top to bottom.
         JPanel variablesListPane = new JPanel();
         variablesListPane.setLayout(new BoxLayout(variablesListPane, BoxLayout.PAGE_AXIS));
-        JLabel variablesLabel = new JLabel("Variables");
 
-        variablesListPane.add(variablesLabel);
         variablesListPane.add(Box.createRigidArea(new Dimension(0,5)));
         variablesListPane.add(variablesListScroller);
-        variablesListPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
-
-        // Add to container
-        container.add(variablesListPane);
-    
-        System.out.println("========ExperimentalVariablesWraper setParentModels()=========");
+        
+        // Container of all the contexts
+        // Don't set size to this container since it grows 
+        Box contextContainer = Box.createVerticalBox();
+        
+        // Container for interventional variable
+        Box interventionalVarBox = Box.createHorizontalBox();
+        interventionalVarBox.setPreferredSize(new Dimension(400, 20));
+        
+        interventionalVarNameField = new StringTextField("", 10);
+        JButton addInterventionBtn= new JButton("Add");
+        
+        // Add file button listener
+        addInterventionBtn.addActionListener((ActionEvent e) -> {
+            // Add the new interventional variable to each context
+            String varName = getInterventionalVarName();
+            
+            contextAreaList.forEach(contextArea -> {
+                String text = contextArea.getText();
+                contextArea.setText(text + "<br>I_" + varName + "<br>");
+            });
+        });
+        
+        interventionalVarBox.add(new JLabel("Interventional variable name: "));
+        interventionalVarBox.add(interventionalVarNameField);
+        interventionalVarBox.add(Box.createRigidArea(new Dimension(10, 1)));
+        interventionalVarBox.add(addInterventionBtn);
+        // Must use the glue, otherwise the label is not left-aligned
+        interventionalVarBox.add(Box.createHorizontalGlue());
+        
+        // Add interventionalVarBox to contextContainer
+        contextContainer.add(interventionalVarBox);
+        contextContainer.add(Box.createVerticalStrut(10));
+        
+      
+        // Create context for each dataset
         for (DataModel dataModel : dataSets) {
             String dataSetName = dataModel.getName();
             System.out.println(dataSetName);
 
-            // Create context for each dataset
+            // context containeing box
             Box contextBox = Box.createVerticalBox();
-            contextBox.setPreferredSize(new Dimension(450, 140));
-            JLabel contextLabel = new JLabel("Context of " + dataSetName);
+            contextBox.setPreferredSize(new Dimension(400, 120));
+            
+            JLabel contextLabel = new JLabel(dataSetName);
+            
             JTextPane contextArea = new JTextPane();
-            contextArea.setPreferredSize(new Dimension(450, 100));
+//            contextArea.setContentType("text/html");
+//            contextArea.setEditable(true);
+            
+            // Add to list for later use
+            contextAreaList.add(contextArea);
+            
+            JScrollPane contextAreaScroller = new JScrollPane(contextArea);
             
             contextBox.add(contextLabel);
-            contextBox.add(Box.createVerticalStrut(10));
-            contextBox.add(contextArea);
-            contextBox.add(Box.createVerticalStrut(10));
+            contextBox.add(contextAreaScroller);
             
-            // Add to the parent container
-            container.add(contextBox);
+            // Add to the parent contextContainer
+            contextContainer.add(contextBox);
+            contextContainer.add(Box.createVerticalStrut(10));
         }
+        
+        // Provides a scrollable view of the contextContainer
+        JScrollPane contextScroller = new JScrollPane(contextContainer);
+        contextScroller.setPreferredSize(new Dimension(480, 320));
+        
+        String contextContainerBorderTitle = "Context & Interventions";
+        // Use a titled border with 5 px inside padding
+        contextScroller.setBorder(new CompoundBorder(BorderFactory.createTitledBorder(contextContainerBorderTitle), new EmptyBorder(5, 5, 5, 5)));
+        
 
+        // Add to container
+        container.add(variablesListPane);
+        container.add(Box.createVerticalStrut(10));
+        container.add(contextScroller);
+        
         // Adds the specified component to the end of this container.
         add(container, BorderLayout.CENTER);
-        
     }
 
 
@@ -184,5 +244,11 @@ public class ExperimentalVariablesParamsEditor extends JPanel implements Finaliz
     }
 
     //=============================== Private Methods ================================//
-
+    private String getInterventionalVarName() {
+        if (!interventionalVarNameField.getText().isEmpty()) {
+            return interventionalVarNameField.getText();
+        } else {
+            return "";
+        }
+    }
 }
