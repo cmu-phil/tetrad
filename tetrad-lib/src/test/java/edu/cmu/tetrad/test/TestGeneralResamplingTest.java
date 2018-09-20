5b57743f24d90e7e9303b24cb7b9ebbe9485e2d5
chirayukong <chirayu.kong@gmail.com>
2018-09-20 15:25:47 -0400

    Cleanup some codes
    - Made some standard output to be under the verbose parameter
    - Renamed Test unit

--- tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestGeneralResamplingTest.java
+++ tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestGeneralResamplingTest.java
@@ -54,7 +54,7 @@ import org.junit.Test;
  * @author Chirayu Kong Wongchokprasitti, PhD (chw20@pitt.edu)
  *
  */
-public class TestGeneralSubSamplingTest {
+public class TestGeneralResamplingTest {
 
 	/**
 	 * @param args
@@ -113,8 +113,8 @@ public class TestGeneralSubSamplingTest {
 		bootstrapTest.setParameters(parameters);
 		bootstrapTest.setEdgeEnsemble(ResamplingEdgeEnsemble.Highest);
 		Graph resultGraph = bootstrapTest.search();
-		System.out.println("Estimated Graph:");
-		System.out.println(resultGraph.toString());
+		//System.out.println("Estimated Graph:");
+		//System.out.println(resultGraph.toString());
 
 		// Adjacency Confusion Matrix
 		int[][] adjAr = GeneralResamplingTest.getAdjConfusionMatrix(dag, resultGraph);
@@ -143,8 +143,8 @@ public class TestGeneralSubSamplingTest {
 
 		Graph dag = makeDiscreteDAG(numVars, numLatentConfounders, edgesPerNode);
 
-		System.out.println("Truth Graph:");
-		System.out.println(dag.toString());
+		//System.out.println("Truth Graph:");
+		//System.out.println(dag.toString());
 
 		BayesPm pm = new BayesPm(dag, 2, 3);
 		BayesIm im = new MlBayesIm(pm, MlBayesIm.RANDOM);
@@ -169,8 +169,8 @@ public class TestGeneralSubSamplingTest {
 		bootstrapTest.setParameters(parameters);
 		bootstrapTest.setEdgeEnsemble(ResamplingEdgeEnsemble.Highest);
 		Graph resultGraph = bootstrapTest.search();
-		System.out.println("Estimated Graph:");
-		System.out.println(resultGraph.toString());
+		//System.out.println("Estimated Graph:");
+		//System.out.println(resultGraph.toString());
 
 		// Adjacency Confusion Matrix
 		int[][] adjAr = GeneralResamplingTest.getAdjConfusionMatrix(dag, resultGraph);
@@ -201,8 +201,8 @@ public class TestGeneralSubSamplingTest {
 		DagToPag dagToPag = new DagToPag(dag);
 		Graph truePag = dagToPag.convert();
 
-		System.out.println("Truth PAG_of_the_true_DAG Graph:");
-		System.out.println(truePag.toString());
+		//System.out.println("Truth PAG_of_the_true_DAG Graph:");
+		//System.out.println(truePag.toString());
 
 		int[] causalOrdering = new int[numVars];
 
@@ -232,8 +232,8 @@ public class TestGeneralSubSamplingTest {
 		bootstrapTest.setParameters(parameters);
 		bootstrapTest.setEdgeEnsemble(ResamplingEdgeEnsemble.Highest);
 		Graph resultGraph = bootstrapTest.search();
-		System.out.println("Estimated PAG_of_the_true_DAG Graph:");
-		System.out.println(resultGraph.toString());
+		//System.out.println("Estimated PAG_of_the_true_DAG Graph:");
+		//System.out.println(resultGraph.toString());
 
 		// Adjacency Confusion Matrix
 		int[][] adjAr = GeneralResamplingTest.getAdjConfusionMatrix(truePag, resultGraph);
@@ -265,8 +265,8 @@ public class TestGeneralSubSamplingTest {
 		DagToPag dagToPag = new DagToPag(dag);
 		Graph truePag = dagToPag.convert();
 
-		System.out.println("Truth PAG_of_the_true_DAG Graph:");
-		System.out.println(truePag.toString());
+		//System.out.println("Truth PAG_of_the_true_DAG Graph:");
+		//System.out.println(truePag.toString());
 
 		BayesPm pm = new BayesPm(dag, 2, 3);
 		BayesIm im = new MlBayesIm(pm, MlBayesIm.RANDOM);
@@ -292,8 +292,8 @@ public class TestGeneralSubSamplingTest {
 		bootstrapTest.setParameters(parameters);
 		bootstrapTest.setEdgeEnsemble(ResamplingEdgeEnsemble.Highest);
 		Graph resultGraph = bootstrapTest.search();
-		System.out.println("Estimated Bootstrapped PAG_of_the_true_DAG Graph:");
-		System.out.println(resultGraph.toString());
+		//System.out.println("Estimated Bootstrapped PAG_of_the_true_DAG Graph:");
+		//System.out.println(resultGraph.toString());
 
 		// Adjacency Confusion Matrix
 		int[][] adjAr = GeneralResamplingTest.getAdjConfusionMatrix(truePag, resultGraph);
@@ -324,8 +324,8 @@ public class TestGeneralSubSamplingTest {
 		DagToPag dagToPag = new DagToPag(dag);
 		Graph truePag = dagToPag.convert();
 
-		System.out.println("Truth PAG_of_the_true_DAG Graph:");
-		System.out.println(truePag.toString());
+		//System.out.println("Truth PAG_of_the_true_DAG Graph:");
+		//System.out.println(truePag.toString());
 
 		int[] causalOrdering = new int[numVars];
 
@@ -355,8 +355,8 @@ public class TestGeneralSubSamplingTest {
 		bootstrapTest.setEdgeEnsemble(ResamplingEdgeEnsemble.Preserved);
 		//bootstrapTest.setParallelMode(false);
 		Graph resultGraph = bootstrapTest.search();
-		System.out.println("Estimated PAG_of_the_true_DAG Graph:");
-		System.out.println(resultGraph.toString());
+		//System.out.println("Estimated PAG_of_the_true_DAG Graph:");
+		//System.out.println(resultGraph.toString());
 
 		// Adjacency Confusion Matrix
 		int[][] adjAr = GeneralResamplingTest.getAdjConfusionMatrix(truePag, resultGraph);
@@ -388,8 +388,8 @@ public class TestGeneralSubSamplingTest {
 		DagToPag dagToPag = new DagToPag(dag);
 		Graph truePag = dagToPag.convert();
 
-		System.out.println("Truth PAG_of_the_true_DAG Graph:");
-		System.out.println(truePag.toString());
+		//System.out.println("Truth PAG_of_the_true_DAG Graph:");
+		//System.out.println(truePag.toString());
 
 		BayesPm pm = new BayesPm(dag, 2, 3);
 		BayesIm im = new MlBayesIm(pm, MlBayesIm.RANDOM);
@@ -414,8 +414,8 @@ public class TestGeneralSubSamplingTest {
 		bootstrapTest.setParameters(parameters);
 		bootstrapTest.setEdgeEnsemble(ResamplingEdgeEnsemble.Highest);
 		Graph resultGraph = bootstrapTest.search();
-		System.out.println("Estimated Bootstrapped PAG_of_the_true_DAG Graph:");
-		System.out.println(resultGraph.toString());
+		//System.out.println("Estimated Bootstrapped PAG_of_the_true_DAG Graph:");
+		//System.out.println(resultGraph.toString());
 
 		// Adjacency Confusion Matrix
 		int[][] adjAr = GeneralResamplingTest.getAdjConfusionMatrix(truePag, resultGraph);
