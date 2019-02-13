f804ebd1c8012d021f90562a8de31a9b2573d24b
yuanzhou <yuanzhou19@gmail.com>
2019-02-13 16:50:33 -0500

    Determinism - in progress

--- tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/datamanip/DeterminismEditor.java
+++ tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/datamanip/DeterminismEditor.java
@@ -8,17 +8,22 @@ package edu.cmu.tetradapp.editor.datamanip;
 import edu.cmu.tetrad.data.DataModel;
 import edu.cmu.tetrad.data.DataModelList;
 import edu.cmu.tetrad.data.DataSet;
+import edu.cmu.tetrad.graph.Node;
 import edu.cmu.tetrad.graph.NodeVariableType;
 import edu.cmu.tetrad.util.Parameters;
 import edu.cmu.tetradapp.editor.FinalizingParameterEditor;
 import edu.cmu.tetradapp.model.DataWrapper;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -72,23 +77,82 @@ public class DeterminismEditor extends JPanel implements FinalizingParameterEdit
             System.out.println("=======================");
             System.out.println(e.get("status"));
             System.out.println(e.get("value"));
-            interventionVarsBox.add(new JLabel("Status variable: " + e.get("status") + " Value variable: " + e.get("value")));
+            interventionVarsBox.add(new JLabel("Status variable: " + e.get("status") + ", Value variable: " + e.get("value")));
+        });
+
+        // Click button to detect deterministic variables that can be merged
+        JButton detectBtn = new JButton("Detect deterministic variables");
+        detectBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                sourceDataSet.getVariables().forEach(var -> {
+
+                });
+
+                List<Map> deterministicPairs = new LinkedList<>();
+
+                for (Node outerVar : sourceDataSet.getVariables()) {
+                    for (Node innerVar : sourceDataSet.getVariables()) {
+                        if (outerVar != innerVar) {
+                            if (isDeterministic(outerVar, innerVar)) {
+                                Map<Node, Node> varPair = new LinkedHashMap<>();
+                                varPair.put(outerVar, innerVar);
+
+                                // Add to list
+                                deterministicPairs.add(varPair);
+                            }
+                        }
+                    }
+                }
+
+                // By now we have a list of deterministic variable pairs that can be merged
+
+            }
         });
 
         // Add data type box to container
         container.add(new JLabel("Merge deterministic variables"));
 
         container.add(domainVarsBox);
-        
+
         container.add(interventionVarsBox);
 
         // Adds the specified component to the end of this container.
         add(container, BorderLayout.CENTER);
     }
 
+    /**
+     * Determine if variable x and y are deterministic (can be merged)
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    private boolean isDeterministic(Node x, Node y) {
+        Map<Object, Object> map = new HashMap<>();
+
+        int xColumnIndex = sourceDataSet.getColumn(x);
+        int yColumnIndex = sourceDataSet.getColumn(y);
+        int numRows = sourceDataSet.getNumRows();
+
+        for (int i = 0; i < numRows; i++) {
+            // objX as key, and objY as value
+            Object objX = sourceDataSet.getObject(i, xColumnIndex);
+            Object objY = sourceDataSet.getObject(i, yColumnIndex);
+
+            if (map.containsKey(objX)) {
+                if (map.get(objX) != objY) {
+                    return false;
+                }
+            } else {
+                map.put(objX, objY);
+            }
+        }
+
+        return true;
+    }
+
     private void groupVariables() {
         sourceDataSet.getVariables().forEach(e -> {
-            // For tiers
             if (e.getNodeVariableType() == NodeVariableType.INTERVENTION_STATUS) {
                 interventionalVars.add(e.getName());
 
