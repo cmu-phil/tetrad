a4d488a3f22a0ec2f78fba3a0c06eefa1c3402b3
jdramsey <jdramsey@andrew.cmu.edu>
2019-04-22 15:11:33 -0400

    Adding disabled fcimax wrapper.

---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pag/FciMax.java
@@ -0,0 +1,173 @@
+package edu.cmu.tetrad.algcomparison.algorithm.oracle.pag;
+
+import edu.cmu.tetrad.algcomparison.algorithm.Algorithm;
+import edu.cmu.tetrad.algcomparison.independence.IndependenceWrapper;
+import edu.cmu.tetrad.algcomparison.utils.HasKnowledge;
+import edu.cmu.tetrad.algcomparison.utils.TakesIndependenceWrapper;
+import edu.cmu.tetrad.algcomparison.utils.TakesInitialGraph;
+import edu.cmu.tetrad.annotation.AlgType;
+import edu.cmu.tetrad.data.*;
+import edu.cmu.tetrad.graph.EdgeListGraph;
+import edu.cmu.tetrad.graph.Graph;
+import edu.cmu.tetrad.search.DagToPag2;
+import edu.cmu.tetrad.util.Parameters;
+import edu.pitt.dbmi.algo.resampling.GeneralResamplingTest;
+import edu.pitt.dbmi.algo.resampling.ResamplingEdgeEnsemble;
+
+import java.util.List;
+
+/**
+ * FCI.
+ *
+ * @author jdramsey
+ */
+//@edu.cmu.tetrad.annotation.Algorithm(
+//        name = "FCI-Max",
+//        command = "fcimax",
+//        algoType = AlgType.allow_latent_common_causes
+//)
+public class FciMax implements Algorithm, TakesInitialGraph, HasKnowledge, TakesIndependenceWrapper {
+
+    static final long serialVersionUID = 23L;
+    private IndependenceWrapper test;
+    private Algorithm algorithm = null;
+    private Graph initialGraph = null;
+    private IKnowledge knowledge = new Knowledge2();
+
+    public FciMax() {
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public FciMax(IndependenceWrapper test) {
+        this.test = test;
+    }
+
+    public FciMax(IndependenceWrapper test, Algorithm algorithm) {
+        this.test = test;
+        this.algorithm = algorithm;
+    }
+
+    @Override
+    public Graph search(DataModel dataSet, Parameters parameters) {
+    	if (parameters.getInt("numberResampling") < 1) {
+            if (algorithm != null) {
+                initialGraph = algorithm.search(dataSet, parameters);
+            }
+
+            edu.cmu.tetrad.search.FciMax search = new edu.cmu.tetrad.search.FciMax(test.getTest(dataSet, parameters));
+            search.setDepth(parameters.getInt("depth"));
+            search.setKnowledge(knowledge);
+            search.setMaxPathLength(parameters.getInt("maxPathLength"));
+            search.setCompleteRuleSetUsed(parameters.getBoolean("completeRuleSetUsed"));
+            search.setVerbose(parameters.getBoolean("verbose"));
+
+//            if (initialGraph != null) {
+//                search.setInitialGraph(initialGraph);
+//            }
+            return search.search();
+        } else {
+            FciMax algorithm = new FciMax(test);
+            //algorithm.setKnowledge(knowledge);
+//          if (initialGraph != null) {
+//      		algorithm.setInitialGraph(initialGraph);
+//  		}
+
+            DataSet data = (DataSet) dataSet;
+            GeneralResamplingTest search = new GeneralResamplingTest(data, algorithm, parameters.getInt("numberResampling"));
+            search.setKnowledge(knowledge);
+            
+            search.setPercentResampleSize(parameters.getDouble("percentResampleSize"));
+            search.setResamplingWithReplacement(parameters.getBoolean("resamplingWithReplacement"));
+            
+            ResamplingEdgeEnsemble edgeEnsemble = ResamplingEdgeEnsemble.Highest;
+            switch (parameters.getInt("resamplingEnsemble", 1)) {
+                case 0:
+                    edgeEnsemble = ResamplingEdgeEnsemble.Preserved;
+                    break;
+                case 1:
+                    edgeEnsemble = ResamplingEdgeEnsemble.Highest;
+                    break;
+                case 2:
+                    edgeEnsemble = ResamplingEdgeEnsemble.Majority;
+            }
+            search.setEdgeEnsemble(edgeEnsemble);
+            search.setAddOriginalDataset(parameters.getBoolean("addOriginalDataset"));
+            
+            search.setParameters(parameters);
+            search.setVerbose(parameters.getBoolean("verbose"));
+            return search.search();
+        }
+    }
+
+    @Override
+    public Graph getComparisonGraph(Graph graph) {
+        return new DagToPag2(new EdgeListGraph(graph)).convert();
+    }
+
+    public String getDescription() {
+        return "FCI (Fast Causal Inference) using " + test.getDescription()
+                + (algorithm != null ? " with initial graph from "
+                        + algorithm.getDescription() : "");
+    }
+
+    @Override
+    public DataType getDataType() {
+        return test.getDataType();
+    }
+
+    @Override
+    public List<String> getParameters() {
+        List<String> parameters = test.getParameters();
+        parameters.add("depth");
+        parameters.add("maxPathLength");
+        parameters.add("completeRuleSetUsed");
+        // Resampling
+        parameters.add("numberResampling");
+        parameters.add("percentResampleSize");
+        parameters.add("resamplingWithReplacement");
+        parameters.add("resamplingEnsemble");
+        parameters.add("addOriginalDataset");
+        parameters.add("verbose");
+        return parameters;
+    }
+
+    @Override
+    public IKnowledge getKnowledge() {
+        return knowledge;
+    }
+
+    @Override
+    public void setKnowledge(IKnowledge knowledge) {
+        this.knowledge = knowledge;
+    }
+
+    @Override
+    public Graph getInitialGraph() {
+        return initialGraph;
+    }
+
+    @Override
+    public void setInitialGraph(Graph initialGraph) {
+        this.initialGraph = initialGraph;
+    }
+
+    @Override
+    public void setInitialGraph(Algorithm algorithm) {
+        this.algorithm = algorithm;
+    }
+
+    @Override
+    public void setIndependenceWrapper(IndependenceWrapper test) {
+        this.test = test;
+    }
+
+    @Override
+    public IndependenceWrapper getIndependenceWrapper() {
+        return test;
+    }
+
+}
