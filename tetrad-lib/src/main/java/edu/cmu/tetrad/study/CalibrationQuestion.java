96642a7c84b1ac689472b6f1771f26904bfe8a0f
jdramsey <jdramsey@andrew.cmu.edu>
2020-01-20 15:38:09 -0500

    Adjusting code a little--Comparison works here for the examples in algcomparison/examples.

---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/study/CalibrationQuestion.java
@@ -0,0 +1,148 @@
+package edu.cmu.tetrad.study;
+
+import edu.cmu.tetrad.data.ContinuousVariable;
+import edu.cmu.tetrad.data.DataSet;
+import edu.cmu.tetrad.graph.Graph;
+import edu.cmu.tetrad.graph.GraphUtils;
+import edu.cmu.tetrad.graph.Node;
+import edu.cmu.tetrad.sem.SemIm;
+import edu.cmu.tetrad.sem.SemPm;
+import edu.cmu.tetrad.util.RandomUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalibrationQuestion {
+
+    public static void main(String... args) {
+        scenario2();
+    }
+
+    private static void scenario1() {
+        int c = 0;
+        int total = 0;
+        int sampleSize = 1000;
+        int numRuns = 1000;
+        int numVars = 10;
+        int avgDegree = 4;
+        int numEdges = avgDegree * numVars / 2;
+        double p = avgDegree / (double) (numVars - 1);
+
+        System.out.println("p = " + p);
+
+        for (int i = 0; i < numRuns; i++) {
+            Node x = new ContinuousVariable("X");
+            Node y = new ContinuousVariable("Y");
+            Node z = new ContinuousVariable("Z");
+
+            List<Node> nodes = new ArrayList<>();
+            nodes.add(x);
+            nodes.add(y);
+            nodes.add(z);
+
+            for (int n = 3; n <= numVars; n++) {
+                nodes.add(new ContinuousVariable("V" + n));
+            }
+
+            Graph gt = GraphUtils.randomGraph(nodes, 0, numEdges, avgDegree, 100, 100, false);
+
+            gt.removeEdge(x, y);
+            gt.removeEdge(y, z);
+            gt.removeEdge(x, z);
+
+
+            gt.addDirectedEdge(x, y);
+            gt.addDirectedEdge(y, z);
+
+            if (RandomUtil.getInstance().nextDouble() <= p) {
+                gt.addDirectedEdge(x, z);
+            }
+
+            SemPm pm = new SemPm(gt);
+            SemIm im = new SemIm(pm);
+
+            DataSet data = im.simulateData(sampleSize, false);
+
+            edu.cmu.tetrad.search.Fges fges = new edu.cmu.tetrad.search.Fges(new edu.cmu.tetrad.search.SemBicScore(data));
+
+            Graph ge = fges.search();
+            ge = GraphUtils.replaceNodes(ge, gt.getNodes());
+
+            System.out.println("gt = " + gt + " ge = " + ge);
+
+            if (ge.isAdjacentTo(x, y) && ge.isAdjacentTo(y, z)//) {// && gt.isAdjacentTo(x, y) && gt.isAdjacentTo(y, z)) {
+//                    && !ge.isAdjacentTo(x, z)
+            ) {
+                if (gt.isAdjacentTo(x, z)) {
+                    c++;
+                }
+
+                total++;
+            }
+
+            System.out.println("c = " + c + " total = " + total + " q = " + (c / (double) total));
+
+        }
+
+        System.out.println("p = " + p + " q = " + (c / (double) total) + " numEdges = " + numEdges);
+    }
+
+    private static void scenario2() {
+        int c = 0;
+        int total = 0;
+        int sampleSize = 1000;
+        int numRuns = 2000;
+        int numVars = 30;
+        int avgDegree = 4;
+        int numEdges = avgDegree * numVars / 2;
+        double p = avgDegree / (double) (numVars - 1);
+
+        System.out.println("p = " + p);
+
+        for (int i = 0; i < numRuns; i++) {
+            Node x = new ContinuousVariable("X");
+            Node y = new ContinuousVariable("Y");
+            Node z = new ContinuousVariable("Z");
+
+            List<Node> nodes = new ArrayList<>();
+            nodes.add(x);
+            nodes.add(y);
+            nodes.add(z);
+
+            for (int n = 3; n <= numVars; n++) {
+                nodes.add(new ContinuousVariable("V" + n));
+            }
+
+            Graph gt = GraphUtils.randomGraph(nodes, 0, numEdges, 100, 100, 100, false);
+
+//            if (gt.isAdjacentTo(x, y) && gt.isAdjacentTo(y, z)) {
+
+            SemPm pm = new SemPm(gt);
+            SemIm im = new SemIm(pm);
+
+            DataSet data = im.simulateData(sampleSize, false);
+
+            edu.cmu.tetrad.search.Fges fges = new edu.cmu.tetrad.search.Fges(new edu.cmu.tetrad.search.SemBicScore(data));
+
+            Graph ge = fges.search();
+            ge = GraphUtils.replaceNodes(ge, gt.getNodes());
+
+            if (ge.isAdjacentTo(x, y) && ge.isAdjacentTo(y, z)// && gt.isAdjacentTo(x, y) && gt.isAdjacentTo(y, z)
+            ) {
+
+
+                if (gt.isAdjacentTo(x, z)) {
+                    c++;
+                }
+
+                total++;
+
+                System.out.println("Run " + i + " p = " + p + " c = " + c + " total = " + total + " q = " + (c / (double) total));
+            }
+
+        }
+//        }
+
+        System.out.println("p = " + p + " q = " + (c / (double) total));
+    }
+}
