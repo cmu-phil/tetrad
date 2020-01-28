ef2cc90bfc3719d52422b6dcaca9c1b928dc6e6f
jdramsey <jdramsey@andrew.cmu.edu>
2020-01-28 16:31:39 -0500

    Adjusting code a little--Comparison works here for the examples in algcomparison/examples.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/study/CalibrationQuestion.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/study/CalibrationQuestion.java
@@ -2,20 +2,19 @@ package edu.cmu.tetrad.study;
 
 import edu.cmu.tetrad.data.ContinuousVariable;
 import edu.cmu.tetrad.data.DataSet;
-import edu.cmu.tetrad.graph.Graph;
-import edu.cmu.tetrad.graph.GraphUtils;
-import edu.cmu.tetrad.graph.Node;
+import edu.cmu.tetrad.graph.*;
 import edu.cmu.tetrad.sem.SemIm;
 import edu.cmu.tetrad.sem.SemPm;
+import edu.cmu.tetrad.util.ChoiceGenerator;
+import edu.cmu.tetrad.util.Parameters;
 import edu.cmu.tetrad.util.RandomUtil;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CalibrationQuestion {
 
     public static void main(String... args) {
-        scenario2();
+        scenario6();
     }
 
     private static void scenario1() {
@@ -91,8 +90,8 @@ public class CalibrationQuestion {
         int c = 0;
         int total = 0;
         int sampleSize = 1000;
-        int numRuns = 2000;
-        int numVars = 30;
+        int numRuns = 500;
+        int numVars = 80;
         int avgDegree = 4;
         int numEdges = avgDegree * numVars / 2;
         double p = avgDegree / (double) (numVars - 1);
@@ -123,14 +122,12 @@ public class CalibrationQuestion {
             DataSet data = im.simulateData(sampleSize, false);
 
             edu.cmu.tetrad.search.Fges fges = new edu.cmu.tetrad.search.Fges(new edu.cmu.tetrad.search.SemBicScore(data));
+//            edu.cmu.tetrad.search.Pc fges = new edu.cmu.tetrad.search.Pc(new edu.cmu.tetrad.search.IndTestFisherZ(data, 0.1));
 
             Graph ge = fges.search();
             ge = GraphUtils.replaceNodes(ge, gt.getNodes());
 
-            if (ge.isAdjacentTo(x, y) && ge.isAdjacentTo(y, z)// && gt.isAdjacentTo(x, y) && gt.isAdjacentTo(y, z)
-            ) {
-
-
+            if (ge.isAdjacentTo(x, y) && ge.isAdjacentTo(y, z) && ge.isAdjacentTo(x, z)) {
                 if (gt.isAdjacentTo(x, z)) {
                     c++;
                 }
@@ -142,6 +139,340 @@ public class CalibrationQuestion {
 
         }
 //        }
+
+        System.out.println("p = " + p + " q = " + (c / (double) total));
+    }
+
+    private static void scenario3() {
+        int c = 0;
+        int d = 0;
+        int totalc = 0;
+        int totald = 0;
+        int sampleSize = 1000;
+        int numRuns = 500;
+        int numVars = 30;
+        int avgDegree = 4;
+        int numEdges = avgDegree * numVars / 2;
+        double p = avgDegree / (double) (numVars - 1);
+
+        System.out.println("p = " + p);
+
+        for (int i = 0; i < numRuns; i++) {
+            Node x = new ContinuousVariable("X");
+            Node y = new ContinuousVariable("Y");
+            Node z = new ContinuousVariable("Z");
+
+            List<Node> nodes = new ArrayList<>();
+            nodes.add(x);
+            nodes.add(y);
+            nodes.add(z);
+
+            for (int n = 3; n <= numVars; n++) {
+                nodes.add(new ContinuousVariable("V" + n));
+            }
+
+            Graph gt = GraphUtils.randomGraph(nodes, 0, numEdges, 100, 100, 100, false);
+
+//            if (!gt.isAdjacentTo(x, y)) gt.addDirectedEdge(x, y);
+//            if (!gt.isAdjacentTo(y, z)) gt.addDirectedEdge(y, z);
+//            if (!gt.isAdjacentTo(x, z)) gt.addDirectedEdge(x, z);
+
+//            gt.removeEdge(x, z);
+
+
+//            if (gt.isAdjacentTo(x, y) && gt.isAdjacentTo(y, z)) {
+
+            SemPm pm = new SemPm(gt);
+
+            Parameters parameters = new Parameters();
+            parameters.set("coefLow", 0.1);
+            parameters.set("coefHigh", 0.5);
+
+            SemIm im = new SemIm(pm, parameters);
+
+            DataSet data = im.simulateData(sampleSize, false);
+
+            edu.cmu.tetrad.search.Fges s = new edu.cmu.tetrad.search.Fges(new edu.cmu.tetrad.search.SemBicScore(data));
+//                edu.cmu.tetrad.search.Pc s = new edu.cmu.tetrad.search.Pc(new edu.cmu.tetrad.search.IndTestFisherZ(data, 0.1));
+
+            Graph ge = s.search();
+            ge = GraphUtils.replaceNodes(ge, gt.getNodes());
+
+//            if (!ge.isAdjacentTo(x, y)) ge.addDirectedEdge(x, y);
+//            if (!ge.isAdjacentTo(y, z)) ge.addDirectedEdge(y, z);
+
+
+            {
+                if (ge.isAdjacentTo(x, y) && ge.isAdjacentTo(y, z)) {
+                    c++;
+                }
+
+                totalc++;
+            }
+
+            if (gt.isAdjacentTo(x, z)) {
+                if (ge.isAdjacentTo(x, y) && ge.isAdjacentTo(y, z)) {
+                    d++;
+                }
+
+                totald++;
+            }
+
+//            System.out.println("Run " + i + " p = " + p + " c = " + c + " totalc = " + totalc + " d = " + d
+//                    + " totald = " + totald + " qc = " + (c / (double) totalc) + " qd = " + (d / (double) totald));
+
+
+            System.out.println("Run " + i + " P(XYe & YZe) = " + (c / (double) totalc) + " P(XYe & YZe | XZt) = " + (d / (double) totald));
+        }
+//        }
+
+        System.out.println("p = " + p + " q = " + (c / (double) totalc));
+    }
+
+    private static void scenario4() {
+        int c = 0;
+        int total = 0;
+        int sampleSize = 1000;
+        int numRuns = 1;
+        int numVars = 20;
+        int avgDegree = 10;
+        int numEdges = avgDegree * numVars / 2;
+        double p = avgDegree / (double) (numVars - 1);
+
+        System.out.println("p = " + p);
+
+        for (int i = 0; i < numRuns; i++) {
+            Node x = new ContinuousVariable("X");
+            Node y = new ContinuousVariable("Y");
+            Node z = new ContinuousVariable("Z");
+
+            List<Node> nodes = new ArrayList<>();
+            nodes.add(x);
+            nodes.add(y);
+            nodes.add(z);
+
+            for (int n = 3; n <= numVars; n++) {
+                nodes.add(new ContinuousVariable("V" + n));
+            }
+
+            Graph gt = GraphUtils.randomGraph(nodes, 0, numEdges, 100, 100, 100, false);
+
+//            if (!gt.isAdjacentTo(x, y)) gt.addDirectedEdge(x, y);
+//            if (!gt.isAdjacentTo(y, z)) gt.addDirectedEdge(y, z);
+//            if (!gt.isAdjacentTo(x, z)) gt.addDirectedEdge(x, z);
+
+//            gt.removeEdge(x, z);
+
+
+//            if (gt.isAdjacentTo(x, y) && gt.isAdjacentTo(y, z)) {
+
+            SemPm pm = new SemPm(gt);
+
+            Parameters parameters = new Parameters();
+            parameters.set("coefLow", 0.1);
+            parameters.set("coefHigh", 0.5);
+
+            SemIm im = new SemIm(pm);
+
+            DataSet data = im.simulateData(sampleSize, false);
+
+            edu.cmu.tetrad.search.Fges s = new edu.cmu.tetrad.search.Fges(new edu.cmu.tetrad.search.SemBicScore(data));
+//                edu.cmu.tetrad.search.Pc s = new edu.cmu.tetrad.search.Pc(new edu.cmu.tetrad.search.IndTestFisherZ(data, 0.1));
+
+            Graph ge = s.search();
+            ge = GraphUtils.replaceNodes(ge, gt.getNodes());
+
+//            if (!ge.isAdjacentTo(x, y)) ge.addDirectedEdge(x, y);
+//            if (!ge.isAdjacentTo(y, z)) ge.addDirectedEdge(y, z);
+
+            ChoiceGenerator gen = new ChoiceGenerator(numVars, 3);
+            int[] choice;
+            int t = 0;
+
+            while ((choice = gen.next()) != null) {
+                List<Node> v = GraphUtils.asList(choice, nodes);
+
+                Node v1 = v.get(0);
+                Node v2 = v.get(1);
+                Node v3 = v.get(2);
+
+                if (ge.isAdjacentTo(v1, v2) && ge.isAdjacentTo(v2, v3) && !ge.isAdjacentTo(v1, v3)) {
+                    if (gt.isAdjacentTo(v1, v3)) {
+                        c++;
+                    }
+
+                    total++;
+
+                    System.out.println("Triple " + ++t + " p = " + p + " c = " + c + " total = " + total + " q = " + (c / (double) total));
+
+                }
+
+            }
+        }
+
+        System.out.println("p = " + p + " q = " + (c / (double) total));
+    }
+
+    // Make GT, simulate data, run FGES, yielding GE. Then find a nonredundant set of possible false negative shields ~XZ
+    // touching all UTFP legs in GE and count for how many of these XZt is in GT.
+    private static void scenario5() {
+        int c = 0;
+        int total = 0;
+        int sampleSize = 1000;
+        int numRuns = 1;
+        int numVars = 11;
+        double avgDegree = 2 * 25 / (double) (11);
+        double numEdges = avgDegree * numVars / 2;
+        double p = avgDegree / (double) (numVars - 1);
+
+        System.out.println("p = " + p + " avgDegree = " + avgDegree);
+
+        for (int i = 0; i < numRuns; i++) {
+            Node x = new ContinuousVariable("X");
+            Node y = new ContinuousVariable("Y");
+            Node z = new ContinuousVariable("Z");
+
+            List<Node> nodes = new ArrayList<>();
+            nodes.add(x);
+            nodes.add(y);
+            nodes.add(z);
+
+            for (int n = 3; n <= numVars; n++) {
+                nodes.add(new ContinuousVariable("V" + n));
+            }
+
+            Graph gt = GraphUtils.randomGraph(nodes, 0, (int) numEdges, 100, 100, 100, false);
+
+//            if (!gt.isAdjacentTo(x, y)) gt.addDirectedEdge(x, y);
+//            if (!gt.isAdjacentTo(y, z)) gt.addDirectedEdge(y, z);
+//            if (!gt.isAdjacentTo(x, z)) gt.addDirectedEdge(x, z);
+
+//            gt.removeEdge(x, z);
+
+
+//            if (gt.isAdjacentTo(x, y) && gt.isAdjacentTo(y, z)) {
+
+            SemPm pm = new SemPm(gt);
+
+            Parameters parameters = new Parameters();
+            parameters.set("coefLow", 0.1);
+            parameters.set("coefHigh", 0.5);
+
+            SemIm im = new SemIm(pm);
+
+            DataSet data = im.simulateData(sampleSize, false);
+
+            edu.cmu.tetrad.search.Fges s = new edu.cmu.tetrad.search.Fges(new edu.cmu.tetrad.search.SemBicScore(data));
+//            edu.cmu.tetrad.search.PcStable s = new edu.cmu.tetrad.search.PcStable(new edu.cmu.tetrad.search.IndTestFisherZ(data, 0.01));
+
+            Graph ge = s.search();
+            ge = GraphUtils.replaceNodes(ge, gt.getNodes());
+
+            ChoiceGenerator gen = new ChoiceGenerator(numVars, 3);
+            int[] choice;
+            int t = 0;
+
+            Set<Node> visited = new HashSet<>();
+
+            while ((choice = gen.next()) != null) {
+                List<Node> v = GraphUtils.asList(choice, nodes);
+
+                Node v1 = v.get(0);
+                Node v2 = v.get(1);
+                Node v3 = v.get(2);
+
+                if (ge.isAdjacentTo(v1, v2) && ge.isAdjacentTo(v2, v3) && !ge.isAdjacentTo(v1, v3)) {
+                    if (visited.contains(v1) && visited.contains(v3)) continue;
+
+                    if (gt.isAdjacentTo(v1, v3)) {
+                        c++;
+                    }
+
+                    total++;
+
+                    visited.add(v1);
+                    visited.add(v3);
+
+                    System.out.println("Triple " + ++t + " p = " + p + " c = " + c + " total = " + total + " Q = " + (c / (double) total));
+
+                }
+            }
+        }
+
+        System.out.println("p = " + p + " q = " + (c / (double) total));
+    }
+
+    private static void scenario6() {
+        int c = 0;
+        int total = 0;
+        int sampleSize = 1000;
+        int numRuns = 1;
+        int numVars = 20;
+        double avgDegree = 4;//2 * 25 / (double) (11);
+        double numEdges = avgDegree * numVars / 2;
+        double p = avgDegree / (double) (numVars - 1);
+
+        System.out.println("p = " + p + " avgDegree = " + avgDegree);
+
+        for (int i = 0; i < numRuns; i++) {
+            Node x = new ContinuousVariable("X");
+            Node y = new ContinuousVariable("Y");
+            Node z = new ContinuousVariable("Z");
+
+            List<Node> nodes = new ArrayList<>();
+            nodes.add(x);
+            nodes.add(y);
+            nodes.add(z);
+
+            for (int n = 3; n <= numVars; n++) {
+                nodes.add(new ContinuousVariable("V" + n));
+            }
+
+            Graph ge = GraphUtils.randomGraph(nodes, 0, (int) numEdges, 100, 100, 100, false);
+            Graph gt = new EdgeListGraph(ge.getNodes());
+
+            for (int r = 0; r < nodes.size(); r++) {
+                for (int s = r + 1; s < nodes.size(); s++) {
+                    double p2 = p + RandomUtil.getInstance().nextUniform(-.1, .1);
+                    if (p2 < 0) p2 = 0;
+                    if (p2 > 1) p2 = 1;
+
+                    if (RandomUtil.getInstance().nextDouble() < p2) {
+                        gt.addUndirectedEdge(nodes.get(r), nodes.get(s));
+                    }
+                }
+            }
+
+            ChoiceGenerator gen = new ChoiceGenerator(numVars, 3);
+            int[] choice;
+            int t = 0;
+
+            Set<Node> visited = new HashSet<>();
+
+            while ((choice = gen.next()) != null) {
+                List<Node> v = GraphUtils.asList(choice, nodes);
+
+                Node v1 = v.get(0);
+                Node v2 = v.get(1);
+                Node v3 = v.get(2);
+
+                if (ge.isAdjacentTo(v1, v2) && ge.isAdjacentTo(v2, v3) && !ge.isAdjacentTo(v1, v3)) {
+                    if (visited.contains(v1) && visited.contains(v3)) continue;
+
+                    if (gt.isAdjacentTo(v1, v3)) {
+                        c++;
+                    }
+
+                    total++;
+
+                    visited.add(v1);
+                    visited.add(v3);
+
+                    System.out.println("Triple " + ++t + " p = " + p + " c = " + c + " total = " + total + " Q = " + (c / (double) total));
+
+                }
+            }
+        }
 
         System.out.println("p = " + p + " q = " + (c / (double) total));
     }
