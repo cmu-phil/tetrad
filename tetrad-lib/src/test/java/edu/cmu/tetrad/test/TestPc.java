3925c016873a75931043d5560512d2cdeaf0be97
jdramsey <jdramsey@andrew.cmu.edu>
2020-11-01 08:38:29 -0500

    Calibrated Fisher Z.

--- tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestPc.java
+++ tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestPc.java
@@ -116,7 +116,7 @@ public class TestPc {
         knowledge.addToTier(5, "PUBS");
         knowledge.addToTier(6, "CITES");
 
-        Pc pc = new Pc(new IndTestFisherZ(dataSet, 0.11));
+        Pc pc = new Pc(new IndTestFisherZ(dataSet, 0.05));
         pc.setKnowledge(knowledge);
 
         Graph pattern = pc.search();
@@ -136,7 +136,7 @@ public class TestPc {
 
         System.out.println(pattern);
 
-        assertEquals(pattern, _true);
+        assertEquals(_true, pattern);
     }
 
     /**
