bfb5667ef5cb34de566eb669ec52089a056c6071
jdramsey <jdramsey@andrew.cmu.edu>
2023-01-13 12:02:26 -0500

    Hooked up knowledge in SP (put SP into a separate class to do it). Fixed the getGraph() method of GRaSP, which wasn't returning the MPDAG.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/search/Grasp.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/Grasp.java
@@ -269,13 +269,20 @@ public class Grasp {
 
     @NotNull
     public Graph getGraph(boolean cpDag) {
+//        if (this.scorer == null) throw new IllegalArgumentException("Please run algorithm first.");
+//        Graph graph = this.scorer.getGraph(cpDag);
+//
+//        orientbk(knowledge, graph, variables);
+//        MeekRules meekRules = new MeekRules();
+//        meekRules.setRevertToUnshieldedColliders(false);
+//        meekRules.orientImplied(graph);
+//
+//        NumberFormat nf = NumberFormatUtil.getInstance().getNumberFormat();
+//        graph.addAttribute("score ", nf.format(this.scorer.score()));
+//        return graph;
+
         if (this.scorer == null) throw new IllegalArgumentException("Please run algorithm first.");
         Graph graph = this.scorer.getGraph(cpDag);
-
-        orientbk(knowledge, graph, variables);
-        MeekRules meekRules = new MeekRules();
-        meekRules.setRevertToUnshieldedColliders(false);
-        meekRules.orientImplied(graph);
 
         NumberFormat nf = NumberFormatUtil.getInstance().getNumberFormat();
         graph.addAttribute("score ", nf.format(this.scorer.score()));
