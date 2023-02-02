0dffd8a15fe8cb47ad5582e040fcbc3efebe85a5
jdramsey <jdramsey@andrew.cmu.edu>
2023-02-02 14:56:14 -0500

    Cleanup

---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pag/PagSamplingRfci.java
@@ -0,0 +1,108 @@
+package edu.cmu.tetrad.algcomparison.algorithm.oracle.pag;
+
+import edu.cmu.tetrad.algcomparison.algorithm.Algorithm;
+import edu.cmu.tetrad.algcomparison.independence.IndependenceWrapper;
+import edu.cmu.tetrad.algcomparison.independence.ProbabilisticTest;
+import edu.cmu.tetrad.algcomparison.utils.HasKnowledge;
+import edu.cmu.tetrad.annotation.AlgType;
+import edu.cmu.tetrad.annotation.Experimental;
+import edu.cmu.tetrad.data.*;
+import edu.cmu.tetrad.graph.EdgeListGraph;
+import edu.cmu.tetrad.graph.Graph;
+import static edu.cmu.tetrad.search.SearchGraphUtils.dagToPag;
+import edu.cmu.tetrad.util.Parameters;
+import edu.cmu.tetrad.util.Params;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * Jan 29, 2023 3:45:09 PM
+ *
+ * @author Kevin V. Bui (kvb2univpitt@gmail.com)
+ */
+@edu.cmu.tetrad.annotation.Algorithm(
+        name = "PAG-Sampling-RFCI",
+        command = "pag-sampling-rfci",
+        algoType = AlgType.allow_latent_common_causes
+)
+@Experimental
+public class PagSamplingRfci implements Algorithm, HasKnowledge {
+
+    public static final List<String> RFCI_PARAMETERS = new LinkedList<>();
+    public static final List<String> PROBABILISTIC_TEST_PARAMETERS = new LinkedList<>();
+
+    static {
+        RFCI_PARAMETERS.add(Params.DEPTH);
+        RFCI_PARAMETERS.add(Params.MAX_PATH_LENGTH);
+        RFCI_PARAMETERS.add(Params.COMPLETE_RULE_SET_USED);
+        RFCI_PARAMETERS.add(Params.TIME_LAG);
+
+        PROBABILISTIC_TEST_PARAMETERS.add(Params.NO_RANDOMLY_DETERMINED_INDEPENDENCE);
+        PROBABILISTIC_TEST_PARAMETERS.add(Params.CUTOFF_IND_TEST);
+        PROBABILISTIC_TEST_PARAMETERS.add(Params.PRIOR_EQUIVALENT_SAMPLE_SIZE);
+    }
+
+    static final long serialVersionUID = 23L;
+    private final IndependenceWrapper test = new ProbabilisticTest();
+    private Knowledge knowledge = new Knowledge();
+
+    @Override
+    public Graph search(DataModel dataSet, Parameters parameters) {
+        edu.pitt.dbmi.algo.bayesian.constraint.search.PagSamplingRfci pagSamplingRfci = new edu.pitt.dbmi.algo.bayesian.constraint.search.PagSamplingRfci(DataPersistence.getDiscreteDataSet(dataSet));
+
+        // Rfic parameters
+        pagSamplingRfci.setKnowledge(this.knowledge);
+        pagSamplingRfci.setDepth(parameters.getInt(Params.DEPTH));
+        pagSamplingRfci.setMaxPathLength(parameters.getInt(Params.MAX_PATH_LENGTH));
+        pagSamplingRfci.setCompleteRuleSetUsed(parameters.getBoolean(Params.COMPLETE_RULE_SET_USED));
+        pagSamplingRfci.setNumRandomizedSearchModels(parameters.getInt(Params.NUM_RANDOMIZED_SEARCH_MODELS));
+
+        // ProbabilisticTest parameters
+        pagSamplingRfci.setThreshold(parameters.getBoolean(Params.NO_RANDOMLY_DETERMINED_INDEPENDENCE));
+        pagSamplingRfci.setCutoff(parameters.getDouble(Params.CUTOFF_IND_TEST));
+        pagSamplingRfci.setPriorEquivalentSampleSize(parameters.getDouble(Params.PRIOR_EQUIVALENT_SAMPLE_SIZE));
+
+        pagSamplingRfci.setVerbose(parameters.getBoolean(Params.VERBOSE));
+
+        return pagSamplingRfci.search();
+    }
+
+    @Override
+    public Graph getComparisonGraph(Graph graph) {
+        return dagToPag(new EdgeListGraph(graph));
+    }
+
+    @Override
+    public String getDescription() {
+        return "PAG-Sampling-RFCI " + this.test.getDescription();
+    }
+
+    @Override
+    public DataType getDataType() {
+        return DataType.Discrete;
+    }
+
+    @Override
+    public List<String> getParameters() {
+        List<String> parameters = new LinkedList<>();
+
+        parameters.addAll(RFCI_PARAMETERS);
+        parameters.addAll(PROBABILISTIC_TEST_PARAMETERS);
+        parameters.add(Params.NUM_RANDOMIZED_SEARCH_MODELS);
+        parameters.add(Params.VERBOSE);
+
+        return parameters;
+    }
+
+    @Override
+    public Knowledge getKnowledge() {
+        return this.knowledge;
+    }
+
+    @Override
+    public void setKnowledge(Knowledge knowledge) {
+        this.knowledge = new Knowledge((Knowledge) knowledge);
+    }
+
+}
---
+++ tetrad-lib/src/main/java/edu/pitt/dbmi/algo/bayesian/constraint/search/PagSamplingRfci.java
@@ -0,0 +1,223 @@
+package edu.pitt.dbmi.algo.bayesian.constraint.search;
+
+import edu.cmu.tetrad.data.DataSet;
+import edu.cmu.tetrad.data.Knowledge;
+import edu.cmu.tetrad.graph.Graph;
+import edu.cmu.tetrad.search.GraphSearch;
+import edu.cmu.tetrad.search.IndTestProbabilistic;
+import edu.cmu.tetrad.search.Rfci;
+import edu.cmu.tetrad.search.SearchGraphUtils;
+import edu.cmu.tetrad.util.GraphTools;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * Jan 29, 2023 4:10:52 PM
+ *
+ * @author Kevin V. Bui (kvb2univpitt@gmail.com)
+ */
+public class PagSamplingRfci implements GraphSearch {
+
+    // Rfci parameters
+    private int depth = -1;
+    private int maxPathLength = -1;
+    private boolean completeRuleSetUsed = true;
+
+    // IndTestProbabilistic parameters
+    private boolean threshold;
+    private double cutoff = 0.5;
+    private double priorEquivalentSampleSize = 10;
+
+    private int numRandomizedSearchModels = 10;
+
+    /**
+     * True if verbose output should be printed.
+     */
+    private boolean verbose;
+
+    /**
+     * The background knowledge.
+     */
+    private Knowledge knowledge = new Knowledge();
+
+    private final DataSet dataSet;
+
+    public PagSamplingRfci(DataSet dataSet) {
+        this.dataSet = dataSet;
+    }
+
+    @Override
+    public Graph search() {
+        return GraphTools.createHighEdgeProbabilityGraph(runSearches());
+    }
+
+    List<Callable<Graph>> createSearchTasks(int numOfTasks) {
+        List<Callable<Graph>> callableTasks = new LinkedList<>();
+
+        for (int i = 0; i < numOfTasks; i++) {
+            callableTasks.add(new RfciSearchTask());
+        }
+
+        return callableTasks;
+    }
+
+    private List<Graph> runSearches() {
+        List<Graph> graphs = new LinkedList<>();
+
+        ExecutorService pool = Executors.newFixedThreadPool(numRandomizedSearchModels);
+        try {
+            while (graphs.size() < numRandomizedSearchModels) {
+                List<Callable<Graph>> callableTasks = createSearchTasks(numRandomizedSearchModels - graphs.size());
+                List<Future<Graph>> completedTasks = pool.invokeAll(callableTasks);
+                for (Future<Graph> completedTask : completedTasks) {
+                    try {
+                        Graph graph = completedTask.get();
+                        if (graph != null && SearchGraphUtils.isLegalPag(graph).isLegalPag()) {
+                            graphs.add(graph);
+                        }
+                    } catch (ExecutionException | InterruptedException exception) {
+                        exception.printStackTrace(System.err);
+                    }
+                }
+            }
+        } catch (InterruptedException exception) {
+            exception.printStackTrace(System.err);
+        } finally {
+            shutdownAndAwaitTermination(pool);
+        }
+
+        return graphs;
+    }
+
+    /**
+     * Call shutdown to reject incoming tasks, and then calling shutdownNow, if
+     * necessary, to cancel any lingering tasks.
+     *
+     * @param pool
+     * @see
+     * https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ExecutorService.html
+     */
+    private void shutdownAndAwaitTermination(ExecutorService pool) {
+        pool.shutdown();
+        try {
+            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                pool.shutdownNow();
+                if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.err.println("Pool did not terminate");
+                }
+            }
+        } catch (InterruptedException ie) {
+            pool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public Knowledge getKnowledge() {
+        return this.knowledge;
+    }
+
+    public void setKnowledge(Knowledge knowledge) {
+        this.knowledge = new Knowledge((Knowledge) knowledge);
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    public int getMaxPathLength() {
+        return maxPathLength;
+    }
+
+    public void setMaxPathLength(int maxPathLength) {
+        this.maxPathLength = maxPathLength;
+    }
+
+    public boolean isCompleteRuleSetUsed() {
+        return completeRuleSetUsed;
+    }
+
+    public void setCompleteRuleSetUsed(boolean completeRuleSetUsed) {
+        this.completeRuleSetUsed = completeRuleSetUsed;
+    }
+
+    public int getNumRandomizedSearchModels() {
+        return numRandomizedSearchModels;
+    }
+
+    public void setNumRandomizedSearchModels(int numRandomizedSearchModels) {
+        this.numRandomizedSearchModels = numRandomizedSearchModels;
+    }
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    public boolean isThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(boolean threshold) {
+        this.threshold = threshold;
+    }
+
+    public double getCutoff() {
+        return cutoff;
+    }
+
+    public void setCutoff(double cutoff) {
+        this.cutoff = cutoff;
+    }
+
+    public double getPriorEquivalentSampleSize() {
+        return priorEquivalentSampleSize;
+    }
+
+    public void setPriorEquivalentSampleSize(double priorEquivalentSampleSize) {
+        this.priorEquivalentSampleSize = priorEquivalentSampleSize;
+    }
+
+    private class RfciSearchTask implements Callable<Graph> {
+
+        public RfciSearchTask() {
+        }
+
+        @Override
+        public Graph call() throws Exception {
+            try {
+                IndTestProbabilistic independenceTest = new IndTestProbabilistic(dataSet);
+                independenceTest.setThreshold(threshold);
+                independenceTest.setCutoff(cutoff);
+                independenceTest.setPriorEquivalentSampleSize(priorEquivalentSampleSize);
+
+                Rfci rfci = new Rfci(independenceTest);
+                rfci.setDepth(depth);
+                rfci.setMaxPathLength(maxPathLength);
+                rfci.setCompleteRuleSetUsed(completeRuleSetUsed);
+                rfci.setVerbose(verbose);
+
+                return rfci.search();
+            } catch (Exception exception) {
+                exception.printStackTrace(System.err);
+
+                return null;
+            }
+        }
+
+    }
+
+}
