9355f81efccc1e7bf11d3ecd878b06235748c658
jdramsey <jdramsey@andrew.cmu.edu>
2023-02-08 23:31:36 -0500

    Some duplicate lines.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pag/GFCI.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pag/GFCI.java
@@ -55,16 +55,14 @@ public class GFCI implements Algorithm, HasKnowledge, UsesScoreWrapper, TakesInd
     @Override
     public Graph search(DataModel dataModel, Parameters parameters) {
         if (parameters.getInt(Params.NUMBER_RESAMPLING) < 1) {
-            if (parameters.getInt(Params.NUMBER_RESAMPLING) < 1) {
-                if (parameters.getInt(Params.TIME_LAG) > 0) {
-                    DataSet dataSet = (DataSet) dataModel;
-                    DataSet timeSeries = TimeSeriesUtils.createLagData(dataSet, parameters.getInt(Params.TIME_LAG));
-                    if (dataSet.getName() != null) {
-                        timeSeries.setName(dataSet.getName());
-                    }
-                    dataModel = timeSeries;
-                    knowledge = timeSeries.getKnowledge();
+            if (parameters.getInt(Params.TIME_LAG) > 0) {
+                DataSet dataSet = (DataSet) dataModel;
+                DataSet timeSeries = TimeSeriesUtils.createLagData(dataSet, parameters.getInt(Params.TIME_LAG));
+                if (dataSet.getName() != null) {
+                    timeSeries.setName(dataSet.getName());
                 }
+                dataModel = timeSeries;
+                knowledge = timeSeries.getKnowledge();
             }
 
             GFci search = new GFci(this.test.getTest(dataModel, parameters), this.score.getScore(dataModel, parameters));
