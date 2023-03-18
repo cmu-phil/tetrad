642fadce06e0387e143eb9769ccbecb707350971
jdramsey <jdramsey@andrew.cmu.edu>
2023-03-18 15:28:06 -0400

    Ha! BossNew without the extends operator!

--- tetrad-lib/src/main/java/edu/cmu/tetrad/search/SuborderSearch.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/SuborderSearch.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * @author bryanandrews
+ */
 public interface SuborderSearch {
 
     void searchSuborder(List<Node> prefix, List<Node> suborder, Map<Node, GrowShrinkTree> gsts, int numStarts);
