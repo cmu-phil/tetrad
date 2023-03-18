737ce21fdd2839db04646057f02ee7fb21238d51
jdramsey <jdramsey@andrew.cmu.edu>
2023-03-17 22:24:56 -0400

    Cleanup.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/cpdag/BOSSNEW.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/cpdag/BOSSNEW.java
@@ -8,19 +8,14 @@ import edu.cmu.tetrad.annotation.Bootstrapping;
 import edu.cmu.tetrad.annotation.Experimental;
 import edu.cmu.tetrad.data.DataModel;
 import edu.cmu.tetrad.data.DataType;
-import edu.cmu.tetrad.data.Knowledge;
 import edu.cmu.tetrad.graph.EdgeListGraph;
 import edu.cmu.tetrad.graph.Graph;
-import edu.cmu.tetrad.graph.Node;
-import edu.cmu.tetrad.search.Boss;
-import edu.cmu.tetrad.search.BossDC;
 import edu.cmu.tetrad.search.BossNew;
 import edu.cmu.tetrad.search.Score;
 import edu.cmu.tetrad.util.Parameters;
 import edu.cmu.tetrad.util.Params;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
