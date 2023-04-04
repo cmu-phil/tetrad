0d961d89fbe30a172cedc0c14b40ea7ce13fa058
jdramsey <jdramsey@andrew.cmu.edu>
2023-04-04 11:41:47 -0400

    Re-adding CCD as non-experimental per user requests.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pag/CCD.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pag/CCD.java
@@ -28,7 +28,7 @@ import java.util.List;
         algoType = AlgType.forbid_latent_common_causes
 )
 @Bootstrapping
-@Experimental
+//@Experimental
 public class CCD implements Algorithm, TakesIndependenceWrapper {
     static final long serialVersionUID = 23L;
     private IndependenceWrapper test;
