3a6f8285497e65f3d2c89c180dcb3d91bfda6df5
jdramsey <jdramsey@andrew.cmu.edu>
2023-04-26 06:18:35 -0400

    Did some commenting on the unit test for LiNGAM and LiNG-D.

--- tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestLing.java
+++ tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestLing.java
@@ -38,6 +38,8 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * @author Joseph Ramsey
  */
@@ -45,6 +47,11 @@ public class TestLing {
 
     @Test
     public void test1() {
+
+        // Testing LiNGAM and LiNG-D on a simple 6-node 6-edge example. This
+        // uses Exp(1) non-Gaussian errors and otherwise default parameters.
+        // We're not using bootstrapping yet here, which could make the result
+        // more accurate.
         long seed = 402030204L;
         RandomUtil.getInstance().setSeed(seed);
         System.out.println("Seed = " + seed + "L");
@@ -56,7 +63,6 @@ public class TestLing {
         parameters.set(Params.SIMULATION_ERROR_TYPE, 3); // Exp(1)
         parameters.set(Params.SIMULATION_PARAM1, 1);
 //        parameters.set(Params.SIMULATION_PARAM2, 1);
-        parameters.set(Params.PENALTY_DISCOUNT, 1);
 
         SemSimulation sim = new SemSimulation(new RandomForward());
         sim.createData(parameters, true);
@@ -64,14 +70,18 @@ public class TestLing {
         Graph g = sim.getTrueGraph(0);
         System.out.println("True graph = " + g);
 
+        // First we use ICA to estimate the W matrix.
         Matrix W = Lingam.estimateW(dataSet, 5000, 1e-6, 1.2);
-
         System.out.println("W = " + W);
 
+        // We then apply LiNGAM with a prune factor of .3. We should get a mostly correct DAG
+        // back. The "prune factor" is a threshold for the B Hat matrix below which values are
+        // sent to zero in absolute value, so that only coefficients whose absolute values
+        // exceed the prune factor are reported as edges in the model. Self-loops are not reported
+        // in the printed graphs but are assumed ot exist for purposes of this algorithm. The
+        // B Hat matrices are scaled so that self-loops always have strength 1.
         System.out.println("LiNGAM");
-
         double pruneFactor = 0.3;
-
         System.out.println("Prune factor = " + pruneFactor);
 
         Lingam lingam = new Lingam();
@@ -79,37 +89,44 @@ public class TestLing {
         Graph g2 = lingam.search(W, dataSet.getVariables());
         System.out.println("Lingam graph = " + g2);
 
+        // Next we try LiNG-D.
         System.out.println("LiNG-D");
 
+        // Here we send any small value in W to 0 that has absolute value below a given threshold.
         double wThreshold = 0.5;
         System.out.println("wThreshold = " + wThreshold);
 
         LingD ling = new LingD();
         ling.setWThreshold(wThreshold);
+
+        // We generate pairs of column permutations (solving the constriained N Rooks problem) with their
+        // associated column-permuted W thresholded W matrices. For the constrained N rooks problme we
+        // are allowed to place a "rook" at any position in the thresholded W matrix that is not zero.
         List<PermutationMatrixPair> pairs = ling.search(W);
 
+        // Then for each N Rook solution we print stuff.
         for (PermutationMatrixPair pair : pairs) {
+
+            // We print the B Hat matrix; this is the matrix of coefficients for the implied linear moodel.
             System.out.println("Model = " + LingD.getBHat(pair));
 
+            // We print the corresponding graph.
             Graph graph = LingD.getGraph(pair, dataSet.getVariables());
+
+            System.out.println("Graph = " + graph);
             boolean stable = LingD.isStable(pair);
+
+            // Finally we print a judgment of whether the BHat model is stable and cyclic.
             System.out.println((stable ? "Is Stable" : "Not stable") + " cyclic = " + graph.paths().existsDirectedCycle());
-            System.out.println(graph);
         }
     }
 
     @Test
     public void testNRooks() {
-        int p = 3;
-        boolean[][] allowableBoard = new boolean[p][p];
-        for (boolean[] row : allowableBoard) Arrays.fill(row, true);
-        allowableBoard[0][2] = false;
-        List<int[]> solutions = NRooks.nRooks(allowableBoard);
-        NRooks.printSolutions(solutions);
-    }
 
-    @Test
-    public void testNRooks2() {
+        // Print all N Rook solutions for a board of size 24 with one square marked
+        // as non-allowable. There should be 4 solutions.
+
         int p = 3;
         boolean[][] allowableBoard = new boolean[p][p];
         for (boolean[] row : allowableBoard) Arrays.fill(row, true);
@@ -125,7 +142,13 @@ public class TestLing {
         System.out.println();
 
         List<int[]> solutions = NRooks.nRooks(allowableBoard);
+
+        // Each solution is a permutation of the columns of the board, one integer
+        // for each row indicating where to place the rook in that row.
         NRooks.printSolutions(solutions);
+
+        // There should be 4 solutions.
+        assertEquals(4, solutions.size());
     }
 }
 
