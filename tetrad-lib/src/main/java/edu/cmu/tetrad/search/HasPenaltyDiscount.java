5fd16bf9049fef43f5f1c1cd09d3c0da7fffa4cd
jdramsey <jdramsey@andrew.cmu.edu>
2023-05-04 18:17:01 -0400

    Fixing documentation and doing general cleamup, etc. Putting code up up to standards into a separate package.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/search/HasPenaltyDiscount.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/HasPenaltyDiscount.java
@@ -1,7 +1,8 @@
 package edu.cmu.tetrad.search;
 
 /**
- * Created by jdramsey on 2/21/16.
+ * Interface for an algorithm can can get/set a value for penalty disoucnt.
+ * @author josephramsey
  */
 public interface HasPenaltyDiscount extends Score {
     void setPenaltyDiscount(double penaltyDiscount);
