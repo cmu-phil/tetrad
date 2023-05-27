4d4b9dea05a5a181c3d3bd03540fe44c96421d97
jdramsey <jdramsey@andrew.cmu.edu>
2023-05-27 12:14:05 -0400

    Added a graph tool to strip null edges from graphs that have been put there by the bootstrapping API, so that models for the graphs can be estimated.

---
+++ tetrad-gui/src/main/java/edu/cmu/tetradapp/model/RemoveNullEdgesGraphWrapper.java
@@ -0,0 +1,59 @@
+///////////////////////////////////////////////////////////////////////////////
+// For information as to what this class does, see the Javadoc, below.       //
+// Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,       //
+// 2007, 2008, 2009, 2010, 2014, 2015, 2022 by Peter Spirtes, Richard        //
+// Scheines, Joseph Ramsey, and Clark Glymour.                               //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify      //
+// it under the terms of the GNU General Public License as published by      //
+// the Free Software Foundation; either version 2 of the License, or         //
+// (at your option) any later version.                                       //
+//                                                                           //
+// This program is distributed in the hope that it will be useful,           //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+// GNU General Public License for more details.                              //
+//                                                                           //
+// You should have received a copy of the GNU General Public License         //
+// along with this program; if not, write to the Free Software               //
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA //
+///////////////////////////////////////////////////////////////////////////////
+
+package edu.cmu.tetradapp.model;
+
+import edu.cmu.tetrad.graph.EdgeListGraph;
+import edu.cmu.tetrad.graph.Graph;
+import edu.cmu.tetrad.session.DoNotAddOldModel;
+import edu.cmu.tetrad.util.GraphSampling;
+import edu.cmu.tetrad.util.Parameters;
+import edu.cmu.tetrad.util.TetradLogger;
+
+/**
+ * <p>The bootstrapping API will generate graphs will "null edges"--that is, edges that aren't in the compositite graph
+ * but for which edge statistics are nonetheless available. These null edges are needed so that when the graph is loaded
+ * back into a new Graph box, all of the bootstrapping information is still available. Unfortunately, such graphs cannot be
+ * estiamted, so for estimation the null edges must be stripped from the graph.</p>
+ *
+ * <p>This graph wrapper does this stripping of null edges from the graph.</p>.
+ *
+ * @author josephramsey
+ */
+public class RemoveNullEdgesGraphWrapper extends GraphWrapper implements DoNotAddOldModel {
+    static final long serialVersionUID = 23L;
+
+    public RemoveNullEdgesGraphWrapper(GraphSource source, Parameters parameters) {
+        this(source.getGraph());
+    }
+
+    public RemoveNullEdgesGraphWrapper(Graph graph) {
+        super(GraphSampling.createGraphWithoutNullEdges(graph), "Remove Null Edges from Boostrapping");
+        TetradLogger.getInstance().log("graph", getGraph() + "");
+    }
+
+
+    public static RemoveNullEdgesGraphWrapper serializableInstance() {
+        return new RemoveNullEdgesGraphWrapper(EdgeListGraph.serializableInstance());
+    }
+}
+
+
