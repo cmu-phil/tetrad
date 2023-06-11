6d26dbcf1965d6be917d2839844a8fb9532eae34
jdramsey <jdramsey@andrew.cmu.edu>
2023-06-11 14:10:23 -0400

    Fixed the displayed error for the case where you erroneously try to do bootrapping on a covariance matrix.

--- tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/MarkovCheckEditor.java
+++ tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/MarkovCheckEditor.java
@@ -691,7 +691,9 @@ public class MarkovCheckEditor extends JPanel {
                 "\n" +
                 "If the independence test yields a bump in the score, this score should be negative for independence judgments and positive for dependence judgments. The histogram will reflect this.\n" +
                 "\n" +
-                "Feel free to select all of the data in the tables, copy it, and paste it into a text file or into Excel. This will let you analyze the data yourself.";
+                "Feel free to select all of the data in the tables, copy it, and paste it into a text file or into Excel. This will let you analyze the data yourself.\n" +
+                "\n" +
+                "A note about Markov Blankets: The \"Markov Blanket\" conditioning set choice implements the Markov blanket calculation in a way that is correct for DAGs, CPDAGs, MAGs, and PAGs. For all of these graph types, the list of m-connecting facts in the Faithfulness tab should be empty. it's possible that for some other graph types this list may not be empty (i.e., the Markov blanket calculation may not be correct).";
     }
 
     private void sortByColumn(int sortCol, boolean indep) {
