86e8e2b3f3c08699856d3faeb6d1de003f93fc14
jdramsey <jdramsey@andrew.cmu.edu>
2024-03-09 05:19:08 -0500

    Update handling of parameters in AbstractBootstrapAlgorithm
    The AbstractBootstrapAlgorithm has been updated to include a special case for when the number of resampling is zero. In these cases, the algorithm directly runs the search operation. This avoids unnecessary data sampling procedures when resampling is not required.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/AbstractBootstrapAlgorithm.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/AbstractBootstrapAlgorithm.java
@@ -58,9 +58,11 @@ public abstract class AbstractBootstrapAlgorithm implements Algorithm, ReturnsBo
         if (dataModel instanceof CovarianceMatrix) {
             if (this instanceof TakesCovarianceMatrix) {
                 return runSearch(dataModel, parameters);
-            } else {
+            }  else {
                 throw new IllegalArgumentException("This search cannot take a covariance matrix as input.");
             }
+        } else if (parameters.getInt(Params.NUMBER_RESAMPLING) == 0) {
+            return runSearch(dataModel, parameters);
         }
 
         List<DataSet> dataSets = DataSampling.createDataSamples((DataSet) dataModel, parameters);
