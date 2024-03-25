7faee43f0e046043092fa646a862cf44bb57659b
jdramsey <jdramsey@andrew.cmu.edu>
2024-03-25 13:02:45 -0400

    Update comment about 'clear' parameter in MarkovCheck
    Clarified how the 'clear' parameter works in the 'generateResults' method in MarkovCheck.java. This improves the comprehensibility of the code, making it easier to maintain and debug in the future.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/search/MarkovCheck.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/MarkovCheck.java
@@ -241,7 +241,7 @@ public class MarkovCheck {
      * the result methods. Note that only results for X _||_ Y | Z1,...,Zn are generated, where X and Y are in the
      * independenceNodes list and Z1,...,Zn are in the conditioningNodes list.
      *
-     * @param clear True if the results should be cleared before generating new results; otherwise, the new results are
+     * @param clear True, if the results should be cleared before generating new results; otherwise, the new results are
      *              appended to the existing results.
      * @see #getResults(boolean)
      */
