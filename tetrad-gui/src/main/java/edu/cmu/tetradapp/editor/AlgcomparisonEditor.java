273c5cd63d85bc3dbbfdab7b696b6aac8634d299
jdramsey <jdramsey@andrew.cmu.edu>
2024-04-05 21:26:55 -0400

    Set Dialogs as non-resizable in AlgcomparisonModel
    Changes have been made in multiple dialogs in the AlgcomparisonModel.java to make them non-resizable. The updated dialogs include 'Edit Simulation Parameters', 'Edit Algorithm Parameters', 'Add Simulation' and 'Add Table Column' settings.

--- tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/AlgcomparisonEditor.java
+++ tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/AlgcomparisonEditor.java
@@ -131,7 +131,7 @@ public class AlgcomparisonEditor extends JPanel {
 
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.setTabPlacement(JTabbedPane.BOTTOM);
-        tabbedPane.setPreferredSize(new Dimension(800, 400));
+        setPreferredSize(new Dimension(800, 400));
 
         addSimulationTab(tabbedPane);
         addAlgorithmTab(tabbedPane);
@@ -788,6 +788,7 @@ public class AlgcomparisonEditor extends JPanel {
 
             JDialog dialog = new JDialog(SwingUtilities.getWindowAncestor(this), "Edit Simulation Parameters", Dialog.ModalityType.APPLICATION_MODAL);
             dialog.setLayout(new BorderLayout());
+            dialog.setResizable(false);
 
             // Add your panel to the center of the dialog
             dialog.add(parameterBox, BorderLayout.CENTER);
@@ -887,6 +888,7 @@ public class AlgcomparisonEditor extends JPanel {
 
             JDialog dialog = new JDialog(SwingUtilities.getWindowAncestor(this), "Edit Algorithm Parameters", Dialog.ModalityType.APPLICATION_MODAL);
             dialog.setLayout(new BorderLayout());
+            dialog.setResizable(false);
 
             // Add your panel to the center of the dialog
             dialog.add(new PaddingPanel(this.parameterBox), BorderLayout.CENTER);
@@ -1162,6 +1164,7 @@ public class AlgcomparisonEditor extends JPanel {
             // Create the JDialog. Use the parent frame to make it modal.
             JDialog dialog = new JDialog(SwingUtilities.getWindowAncestor(this), "Add Simulation", Dialog.ModalityType.APPLICATION_MODAL);
             dialog.setLayout(new BorderLayout());
+            dialog.setResizable(false);
 
             // Add your panel to the center of the dialog
             dialog.add(panel, BorderLayout.CENTER);
@@ -1285,6 +1288,7 @@ public class AlgcomparisonEditor extends JPanel {
             JDialog dialog = new JDialog(SwingUtilities.getWindowAncestor(this), "Add Simulation", Dialog.ModalityType.APPLICATION_MODAL);
             dialog.setLayout(new BorderLayout());
             dialog.add(panel, BorderLayout.CENTER);
+            dialog.setResizable(false);
 
             // Create a panel for the buttons
             JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
@@ -1552,6 +1556,7 @@ public class AlgcomparisonEditor extends JPanel {
             JDialog dialog = new JDialog(SwingUtilities.getWindowAncestor(this), "Add Table Column", Dialog.ModalityType.APPLICATION_MODAL);
             dialog.setLayout(new BorderLayout());
             dialog.add(panel, BorderLayout.CENTER);
+            dialog.setResizable(false);
 
             // Create a panel for the buttons
             JPanel buttonPanel = getButtonPanel(columnSelectionTableModel, dialog);
