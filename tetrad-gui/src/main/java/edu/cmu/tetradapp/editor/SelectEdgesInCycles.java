e86b11ec7e6b5f35f17d36dab973097f5cf88e4e
jdramsey <jdramsey@andrew.cmu.edu>
2024-04-19 19:49:29 -0400

    Add 'SelectEdgesInCycles' class in tetrad-gui module
    This commit introduces a new class 'SelectEdgesInCycles' in the 'tetrad-gui' module. This class selects all directed edges in a given display graph presented in the 'GraphWorkbench' workbench. It's mainly used to highlight any cyclic paths within graphs for easier identification.

---
+++ tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/SelectEdgesInCycles.java
@@ -0,0 +1,106 @@
+///////////////////////////////////////////////////////////////////////////////
+// For information as to what this class does, see the Javadoc, below.       //
+// Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,       //
+// 2007, 2008, 2009, 2010, 2014, 2015, 2022 by Peter Spirtes, Richard        //
+// Scheines, Joseph Ramsey, and Clark Glymour.                               //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify      //
+// it under the terms of the GNU General Public License as published by      //
+// the Free Software Foundation; either version 2 of the License, or         //
+// (at your option) any later version.                                       //
+//                                                                           //
+// This program is distributed in the hope that it will be useful,           //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+// GNU General Public License for more details.                              //
+//                                                                           //
+// You should have received a copy of the GNU General Public License         //
+// along with this program; if not, write to the Free Software               //
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA //
+///////////////////////////////////////////////////////////////////////////////
+
+package edu.cmu.tetradapp.editor;
+
+import edu.cmu.tetrad.graph.Edge;
+import edu.cmu.tetrad.graph.Edges;
+import edu.cmu.tetrad.graph.Graph;
+import edu.cmu.tetrad.graph.Node;
+import edu.cmu.tetradapp.workbench.DisplayEdge;
+import edu.cmu.tetradapp.workbench.GraphWorkbench;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.Transferable;
+import java.awt.event.ActionEvent;
+
+/**
+ * Selects all directed edges in the given display graph.
+ *
+ * @author josephramsey
+ * @version $Id: $Id
+ */
+public class SelectEdgesInCycles extends AbstractAction implements ClipboardOwner {
+
+    /**
+     * The desktop containing the target session editor.
+     */
+    private final GraphWorkbench workbench;
+
+    /**
+     * Creates a new copy subsession action for the given desktop and clipboard.
+     *
+     * @param workbench the given workbench.
+     */
+    public SelectEdgesInCycles(GraphWorkbench workbench) {
+        super("Highlight Edges In Cycles");
+
+        if (workbench == null) {
+            throw new NullPointerException("Desktop must not be null.");
+        }
+
+        this.workbench = workbench;
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Selects all directed edges in the given display graph.
+     */
+    public void actionPerformed(ActionEvent e) {
+        this.workbench.deselectAll();
+        Graph graph = this.workbench.getGraph();
+
+        if (graph == null) {
+            JOptionPane.showMessageDialog(this.workbench, "No graph to check for cycles.");
+            return;
+        }
+
+        for (Component comp : this.workbench.getComponents()) {
+            if (comp instanceof DisplayEdge) {
+                Edge edge = ((DisplayEdge) comp).getModelEdge();
+
+                if (Edges.isDirectedEdge(edge)) {
+                    Node x = edge.getNode1();
+                    Node y = edge.getNode2();
+
+                    if (graph.paths().existsDirectedPath(y, x)) {
+                        this.workbench.selectEdge(edge);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Required by the AbstractAction interface; does nothing.
+     */
+    public void lostOwnership(Clipboard clipboard, Transferable contents) {
+    }
+}
+
+
+
