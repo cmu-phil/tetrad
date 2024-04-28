de36601f2d7aab0683b646dc5f9b574f852ce912
jdramsey <jdramsey@andrew.cmu.edu>
2024-04-28 12:23:41 -0400

    Add GraspLvLite class and update GraphUtils
    Added the new GraspLvLite class to the edu.cmu.tetrad.algcomparison.algorithm.oracle.pag package and the edu.cmu.tetrad.search package. This class is oriented to find a PAG with latent variables, using the Grasp algorithm for the first search stage. Modifications were also made in the GraphUtils.java file to adjust endpoints.

---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pag/GraspLvLite.java
@@ -0,0 +1,269 @@
+package edu.cmu.tetrad.algcomparison.algorithm.oracle.pag;
+
+import edu.cmu.tetrad.algcomparison.algorithm.AbstractBootstrapAlgorithm;
+import edu.cmu.tetrad.algcomparison.algorithm.Algorithm;
+import edu.cmu.tetrad.algcomparison.algorithm.ReturnsBootstrapGraphs;
+import edu.cmu.tetrad.algcomparison.algorithm.TakesCovarianceMatrix;
+import edu.cmu.tetrad.algcomparison.independence.IndependenceWrapper;
+import edu.cmu.tetrad.algcomparison.score.ScoreWrapper;
+import edu.cmu.tetrad.algcomparison.utils.HasKnowledge;
+import edu.cmu.tetrad.algcomparison.utils.TakesIndependenceWrapper;
+import edu.cmu.tetrad.algcomparison.utils.UsesScoreWrapper;
+import edu.cmu.tetrad.annotation.AlgType;
+import edu.cmu.tetrad.annotation.Bootstrapping;
+import edu.cmu.tetrad.data.DataModel;
+import edu.cmu.tetrad.data.DataSet;
+import edu.cmu.tetrad.data.DataType;
+import edu.cmu.tetrad.data.Knowledge;
+import edu.cmu.tetrad.graph.Graph;
+import edu.cmu.tetrad.graph.GraphTransforms;
+import edu.cmu.tetrad.search.IndependenceTest;
+import edu.cmu.tetrad.search.score.Score;
+import edu.cmu.tetrad.search.utils.TsUtils;
+import edu.cmu.tetrad.util.Parameters;
+import edu.cmu.tetrad.util.Params;
+
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Adjusts GFCI to use a permutation algorithm (such as BOSS-Tuck) to do the initial steps of finding adjacencies and
+ * unshielded colliders.
+ * <p>
+ * GFCI reference is this:
+ * <p>
+ * J.M. Ogarrio and P. Spirtes and J. Ramsey, "A Hybrid Causal Search Algorithm for Latent Variable Models," JMLR 2016.
+ *
+ * @author josephramsey
+ * @version $Id: $Id
+ */
+@edu.cmu.tetrad.annotation.Algorithm(
+        name = "Graph LV Lite",
+        command = "graph-lv-lite",
+        algoType = AlgType.allow_latent_common_causes
+)
+@Bootstrapping
+public class GraspLvLite extends AbstractBootstrapAlgorithm implements Algorithm, UsesScoreWrapper, TakesIndependenceWrapper,
+        HasKnowledge, ReturnsBootstrapGraphs, TakesCovarianceMatrix {
+
+    @Serial
+    private static final long serialVersionUID = 23L;
+
+    /**
+     * The independence test to use.
+     */
+    private IndependenceWrapper test;
+
+    /**
+     * The score to use.
+     */
+    private ScoreWrapper score;
+
+    /**
+     * The knowledge.
+     */
+    private Knowledge knowledge = new Knowledge();
+
+    /**
+     * <p>Constructor for GraspFci.</p>
+     */
+    public GraspLvLite() {
+        // Used for reflection; do not delete.
+    }
+
+    /**
+     * <p>Constructor for GraspFci.</p>
+     *
+     * @param test  a {@link IndependenceWrapper} object
+     * @param score a {@link ScoreWrapper} object
+     */
+    public GraspLvLite(IndependenceWrapper test, ScoreWrapper score) {
+        this.test = test;
+        this.score = score;
+    }
+
+    /**
+     * Runs a search algorithm to find a graph structure based on a given data set and parameters.
+     *
+     * @param dataModel  the data set to be used for the search algorithm
+     * @param parameters the parameters for the search algorithm
+     * @return the graph structure found by the search algorithm
+     */
+    @Override
+    public Graph runSearch(DataModel dataModel, Parameters parameters) {
+        if (parameters.getInt(Params.TIME_LAG) > 0) {
+            if (!(dataModel instanceof DataSet dataSet)) {
+                throw new IllegalArgumentException("Expecting a dataset for time lagging.");
+            }
+
+            DataSet timeSeries = TsUtils.createLagData(dataSet, parameters.getInt(Params.TIME_LAG));
+            if (dataSet.getName() != null) {
+                timeSeries.setName(dataSet.getName());
+            }
+            dataModel = timeSeries;
+            knowledge = timeSeries.getKnowledge();
+        }
+
+        IndependenceTest test = this.test.getTest(dataModel, parameters);
+        Score score = this.score.getScore(dataModel, parameters);
+
+        test.setVerbose(parameters.getBoolean(Params.VERBOSE));
+        edu.cmu.tetrad.search.GraspLvLite search = new edu.cmu.tetrad.search.GraspLvLite(test, score);
+
+        // GRaSP
+        search.setSeed(parameters.getLong(Params.SEED));
+        search.setDepth(parameters.getInt(Params.GRASP_DEPTH));
+        search.setSingularDepth(parameters.getInt(Params.GRASP_SINGULAR_DEPTH));
+        search.setNonSingularDepth(parameters.getInt(Params.GRASP_NONSINGULAR_DEPTH));
+        search.setOrdered(parameters.getBoolean(Params.GRASP_ORDERED_ALG));
+        search.setUseScore(parameters.getBoolean(Params.GRASP_USE_SCORE));
+        search.setUseRaskuttiUhler(parameters.getBoolean(Params.GRASP_USE_RASKUTTI_UHLER));
+        search.setUseDataOrder(parameters.getBoolean(Params.USE_DATA_ORDER));
+        search.setNumStarts(parameters.getInt(Params.NUM_STARTS));
+
+        // FCI
+        search.setDepth(parameters.getInt(Params.DEPTH));
+        search.setMaxPathLength(parameters.getInt(Params.MAX_PATH_LENGTH));
+        search.setCompleteRuleSetUsed(parameters.getBoolean(Params.COMPLETE_RULE_SET_USED));
+        search.setDoDiscriminatingPathRule(parameters.getBoolean(Params.DO_DISCRIMINATING_PATH_RULE));
+
+        // General
+        search.setVerbose(parameters.getBoolean(Params.VERBOSE));
+        search.setKnowledge(this.knowledge);
+
+        return search.search();
+    }
+
+    /**
+     * Retrieves a comparison graph by transforming a true directed graph into a partially directed graph (PAG).
+     *
+     * @param graph The true directed graph, if there is one.
+     * @return The comparison graph.
+     */
+    @Override
+    public Graph getComparisonGraph(Graph graph) {
+        return GraphTransforms.dagToPag(graph);
+    }
+
+    /**
+     * Returns a short, one-line description of this algorithm. The description is generated by concatenating the
+     * descriptions of the test and score objects associated with this algorithm.
+     *
+     * @return The description of this algorithm.
+     */
+    @Override
+    public String getDescription() {
+        return "GRaSP LV Lite using " + this.test.getDescription()
+               + " and " + this.score.getDescription();
+    }
+
+    /**
+     * Retrieves the data type required by the search algorithm.
+     *
+     * @return The data type required by the search algorithm.
+     */
+    @Override
+    public DataType getDataType() {
+        return this.test.getDataType();
+    }
+
+    /**
+     * Retrieves the list of parameters used by the algorithm.
+     *
+     * @return The list of parameters used by the algorithm.
+     */
+    @Override
+    public List<String> getParameters() {
+        List<String> params = new ArrayList<>();
+
+        // GRaSP
+        params.add(Params.GRASP_DEPTH);
+        params.add(Params.GRASP_SINGULAR_DEPTH);
+        params.add(Params.GRASP_NONSINGULAR_DEPTH);
+        params.add(Params.GRASP_ORDERED_ALG);
+        params.add(Params.GRASP_USE_RASKUTTI_UHLER);
+        params.add(Params.USE_DATA_ORDER);
+        params.add(Params.NUM_STARTS);
+
+        // FCI
+        params.add(Params.DEPTH);
+        params.add(Params.MAX_PATH_LENGTH);
+        params.add(Params.COMPLETE_RULE_SET_USED);
+        params.add(Params.DO_DISCRIMINATING_PATH_RULE);
+        params.add(Params.POSSIBLE_MSEP_DONE);
+
+        // General
+        params.add(Params.TIME_LAG);
+
+        params.add(Params.SEED);
+
+        params.add(Params.VERBOSE);
+
+        return params;
+    }
+
+
+    /**
+     * Retrieves the knowledge object associated with this method.
+     *
+     * @return The knowledge object.
+     */
+    @Override
+    public Knowledge getKnowledge() {
+        return this.knowledge;
+    }
+
+    /**
+     * Sets the knowledge object associated with this method.
+     *
+     * @param knowledge the knowledge object to be set
+     */
+    @Override
+    public void setKnowledge(Knowledge knowledge) {
+        this.knowledge = new Knowledge(knowledge);
+    }
+
+    /**
+     * Retrieves the IndependenceWrapper object associated with this method. The IndependenceWrapper object contains an
+     * IndependenceTest that checks the independence of two variables conditional on a set of variables using a given
+     * dataset and parameters .
+     *
+     * @return The IndependenceWrapper object associated with this method.
+     */
+    @Override
+    public IndependenceWrapper getIndependenceWrapper() {
+        return this.test;
+    }
+
+    /**
+     * Sets the independence wrapper.
+     *
+     * @param test the independence wrapper.
+     */
+    @Override
+    public void setIndependenceWrapper(IndependenceWrapper test) {
+        this.test = test;
+    }
+
+    /**
+     * Retrieves the ScoreWrapper object associated with this method.
+     *
+     * @return The ScoreWrapper object associated with this method.
+     */
+    @Override
+    public ScoreWrapper getScoreWrapper() {
+        return this.score;
+    }
+
+    /**
+     * Sets the score wrapper for the algorithm.
+     *
+     * @param score the score wrapper.
+     */
+    @Override
+    public void setScoreWrapper(ScoreWrapper score) {
+        this.score = score;
+    }
+}
---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/GraspLvLite.java
@@ -0,0 +1,413 @@
+///////////////////////////////////////////////////////////////////////////////
+// For information as to what this class does, see the Javadoc, below.       //i
+// Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,       //
+// 2007, 2008, 2009, 2010, 2014, 2015, 2022 by Peter Spirtes, Richard        //
+// Scheines, Joseph Ramsey, and Clark Glymour.                               //
+//                                                                           //
+// This program is free software; you can redistribute it and/or modify      //
+// it under the terms of the GNU General Public License as published by      //
+// the Free Software Foundation; either version 2 of the License, or         //
+// (at your option) any later version.                                       //
+//                                                                           //
+// This program is distributed in the hope that it will be useful,           //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+// GNU General Public License for more details.                              //
+//                                                                           //
+// You should have received a copy of the GNU General Public License         //
+// along with this program; if not, write to the Free Software               //
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA //
+///////////////////////////////////////////////////////////////////////////////
+package edu.cmu.tetrad.search;
+
+import edu.cmu.tetrad.data.Knowledge;
+import edu.cmu.tetrad.graph.*;
+import edu.cmu.tetrad.search.score.Score;
+import edu.cmu.tetrad.search.utils.FciOrient;
+import edu.cmu.tetrad.search.utils.SepsetProducer;
+import edu.cmu.tetrad.search.utils.SepsetsGreedy;
+import edu.cmu.tetrad.search.utils.TeyssierScorer;
+import edu.cmu.tetrad.util.TetradLogger;
+
+import java.util.List;
+
+/**
+ * Uses GRaSP in place of FGES for the initial step in the GFCI algorithm. This tends to produce a accurate PAG than
+ * GFCI as a result, for the latent variables case. This is a simple substitution; the reference for GFCI is here: J.M.
+ * Ogarrio and P. Spirtes and J. Ramsey, "A Hybrid Causal Search Algorithm for Latent Variable Models," JMLR 2016. Here,
+ * BOSS has been substituted for FGES.
+ * <p>
+ * For the first step, the GRaSP algorithm is used, with the same modifications as in the GFCI algorithm.
+ * <p>
+ * For the second step, the FCI final orientation algorithm is used, with the same modifications as in the GFCI
+ * algorithm.
+ * <p>
+ * For GRaSP only a score is needed, but there are steps in GFCI that require a test, so for this method, both a test
+ * and a score need to be given.
+ * <p>
+ * This class is configured to respect knowledge of forbidden and required edges, including knowledge of temporal
+ * tiers.
+ *
+ * @author josephramsey
+ * @author bryanandrews
+ * @version $Id: $Id
+ * @see Grasp
+ * @see GFci
+ * @see FciOrient
+ * @see Knowledge
+ */
+public final class GraspLvLite implements IGraphSearch {
+
+    /**
+     * The conditional independence test.
+     */
+    private final IndependenceTest independenceTest;
+    /**
+     * The logger to use.
+     */
+    private final TetradLogger logger = TetradLogger.getInstance();
+    /**
+     * The score.
+     */
+    private final Score score;
+    /**
+     * The background knowledge.
+     */
+    private Knowledge knowledge = new Knowledge();
+    /**
+     * Flag for the complete rule set, true if one should use the complete rule set, false otherwise.
+     */
+    private boolean completeRuleSetUsed = true;
+    /**
+     * The maximum length for any discriminating path. -1 if unlimited; otherwise, a positive integer.
+     */
+    private int maxPathLength = -1;
+    /**
+     * True iff verbose output should be printed.
+     */
+    private boolean verbose;
+    /**
+     * The number of starts for GRaSP.
+     */
+    private int numStarts = 1;
+    /**
+     * Whether to use Raskutti and Uhler's modification of GRaSP.
+     */
+    private boolean useRaskuttiUhler = false;
+    /**
+     * Whether to use data order.
+     */
+    private boolean useDataOrder = true;
+    /**
+     * Whether to use score.
+     */
+    private boolean useScore = true;
+    /**
+     * Whether to use the discriminating path rule.
+     */
+    private boolean doDiscriminatingPathRule = true;
+    /**
+     * Whether to use the ordered version of GRaSP.
+     */
+    private boolean ordered = false;
+    /**
+     * The depth for GRaSP.
+     */
+    private int depth = -1;
+    /**
+     * The depth for singular variables.
+     */
+    private int uncoveredDepth = 1;
+    /**
+     * The depth for non-singular variables.
+     */
+    private int nonSingularDepth = 1;
+    /**
+     * The seed used for random number generation. If the seed is not set explicitly, it will be initialized with a
+     * value of -1. The seed is used for producing the same sequence of random numbers every time the program runs.
+     *
+     * @see GraspLvLite#setSeed(long)
+     */
+    private long seed = -1;
+
+    /**
+     * Constructs a new GraspFci object.
+     *
+     * @param test  The independence test.
+     * @param score a {@link Score} object
+     */
+    public GraspLvLite(IndependenceTest test, Score score) {
+        if (score == null) {
+            throw new NullPointerException();
+        }
+
+        this.score = score;
+        this.independenceTest = test;
+    }
+
+    /**
+     * Run the search and return s a PAG.
+     *
+     * @return The PAG.
+     */
+    public Graph search() {
+        List<Node> nodes = this.independenceTest.getVariables();
+
+        if (nodes == null) {
+            throw new NullPointerException("Nodes from test were null.");
+        }
+
+        if (verbose) {
+            TetradLogger.getInstance().forceLogMessage("Starting Grasp-FCI algorithm.");
+            TetradLogger.getInstance().forceLogMessage("Independence test = " + this.independenceTest + ".");
+        }
+
+        // The PAG being constructed.
+        // Run GRaSP to get a CPDAG (like GFCI with FGES)...
+        Grasp alg = new Grasp(independenceTest, score);
+        alg.setSeed(seed);
+        alg.setOrdered(ordered);
+        alg.setUseScore(useScore);
+        alg.setUseRaskuttiUhler(useRaskuttiUhler);
+        alg.setUseDataOrder(useDataOrder);
+        int graspDepth = 3;
+        alg.setDepth(graspDepth);
+        alg.setUncoveredDepth(uncoveredDepth);
+        alg.setNonSingularDepth(nonSingularDepth);
+        alg.setNumStarts(numStarts);
+        alg.setVerbose(verbose);
+
+        List<Node> variables = this.score.getVariables();
+        assert variables != null;
+
+        List<Node> best = alg.bestOrder(variables);
+//        Graph graph = alg.getGraph(true);
+//        Graph _graph = new EdgeListGraph(graph);
+//        _graph.reorientAllWith(Endpoint.CIRCLE);
+
+        TeyssierScorer teyssierScorer = new TeyssierScorer(independenceTest, score);
+        teyssierScorer.score(best);
+        Graph graph = teyssierScorer.getGraph(false);
+        Graph _graph = new EdgeListGraph(graph);
+        _graph.reorientAllWith(Endpoint.CIRCLE);
+
+        for (int i = 0; i < best.size(); i++) {
+            for (int j = i + 1; j < best.size(); j++) {
+                for (int k = j + 1; k < best.size(); k++) {
+                    Node a = best.get(i);
+                    Node b = best.get(j);
+                    Node c = best.get(k);
+
+                    if (graph.isAdjacentTo(a, c) && graph.isAdjacentTo(b, c) && !graph.isAdjacentTo(a, b)) {
+                        _graph.setEndpoint(a, c, Endpoint.ARROW);
+                        _graph.setEndpoint(b, c, Endpoint.ARROW);
+                    }
+                }
+            }
+        }
+
+        teyssierScorer.score(best);
+        Graph __graph = teyssierScorer.getGraph(true);
+
+        // Look for every triangle in graph A->C, B->C, A->B
+        for (int i = 0; i < best.size(); i++) {
+            for (int j = i + 1; j < best.size(); j++) {
+                for (int k = j + 1; k < best.size(); k++) {
+                    Node a = best.get(i);
+                    Node b = best.get(j);
+                    Node c = best.get(k);
+
+                    double score = teyssierScorer.score(best);
+
+                    if (__graph.isAdjacentTo(a, c) && __graph.isAdjacentTo(b, c) && __graph.isAdjacentTo(a, b)) {
+                        if (__graph.getEdge(a, b).isDirected() && __graph.getEdge(b, c).isDirected()
+                            && __graph.getEdge(a, c).isDirected()) {
+                            teyssierScorer.tuck(a, best.indexOf(b));
+                            if (teyssierScorer.score() > score - 0.01) {
+                                _graph.removeEdge(a, c);
+                                _graph.setEndpoint(c, b, Endpoint.ARROW);
+                            }
+                        }
+
+
+//                        teyssierScorer.tuck(c, best.indexOf(a));
+//                        if (teyssierScorer.score() > score - 0.01) {
+////                            graph.removeEdge(a, c);
+//                            graph.removeEdge(b, c);
+//                            graph.addBidirectedEdge(b, c);
+//                        }
+                    }
+                }
+            }
+        }
+
+        graph = _graph;
+
+//        if (true) {
+//            return graph;
+//        }
+
+//        Graph referenceDag = new EdgeListGraph(graph);
+//
+//        // GFCI extra edge removal step...
+        SepsetProducer sepsets = new SepsetsGreedy(graph, this.independenceTest, null, this.depth, knowledge);
+//        SepsetProducer sepsets;
+//
+//        if (independenceTest instanceof MsepTest) {
+//            sepsets = new DagSepsets(((MsepTest) independenceTest).getGraph());
+//        } else {
+//            sepsets = new SepsetsGreedy(graph, this.independenceTest, null, this.depth, knowledge);
+//        }
+//
+//        gfciExtraEdgeRemovalStep(graph, referenceDag, nodes, sepsets, verbose);
+//        GraphUtils.gfciR0(graph, referenceDag, sepsets, knowledge, verbose);
+
+        FciOrient fciOrient = new FciOrient(sepsets);
+        fciOrient.setCompleteRuleSetUsed(completeRuleSetUsed);
+        fciOrient.setDoDiscriminatingPathColliderRule(true);
+        fciOrient.setDoDiscriminatingPathTailRule(true);
+        fciOrient.setVerbose(verbose);
+        fciOrient.setKnowledge(knowledge);
+        fciOrient.doFinalOrientation(graph);
+
+        GraphUtils.replaceNodes(graph, this.independenceTest.getVariables());
+
+        graph = GraphTransforms.zhangMagFromPag(graph);
+        graph = GraphTransforms.dagToPag(graph);
+
+        return graph;
+    }
+
+    /**
+     * Sets the knowledge used in search.
+     *
+     * @param knowledge This knowledge.
+     */
+    public void setKnowledge(Knowledge knowledge) {
+        this.knowledge = new Knowledge(knowledge);
+    }
+
+    /**
+     * Sets whether Zhang's complete rules set is used.
+     *
+     * @param completeRuleSetUsed set to true if Zhang's complete rule set should be used, false if only R1-R4 (the rule
+     *                            set of the original FCI) should be used. False by default.
+     */
+    public void setCompleteRuleSetUsed(boolean completeRuleSetUsed) {
+        this.completeRuleSetUsed = completeRuleSetUsed;
+    }
+
+    /**
+     * Sets the maximum length of any discriminating path searched.
+     *
+     * @param maxPathLength the maximum length of any discriminating path, or -1 if unlimited.
+     */
+    public void setMaxPathLength(int maxPathLength) {
+        if (maxPathLength < -1) {
+            throw new IllegalArgumentException("Max path length must be -1 (unlimited) or >= 0: " + maxPathLength);
+        }
+
+        this.maxPathLength = maxPathLength;
+    }
+
+    /**
+     * Sets whether verbose output should be printed.
+     *
+     * @param verbose True, if so.
+     */
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+    /**
+     * Sets the number of starts for GRaSP.
+     *
+     * @param numStarts The number of starts.
+     */
+    public void setNumStarts(int numStarts) {
+        this.numStarts = numStarts;
+    }
+
+    /**
+     * Sets the depth for GRaSP.
+     *
+     * @param depth The depth.
+     */
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+
+    /**
+     * Sets whether to use Raskutti and Uhler's modification of GRaSP.
+     *
+     * @param useRaskuttiUhler True, if so.
+     */
+    public void setUseRaskuttiUhler(boolean useRaskuttiUhler) {
+        this.useRaskuttiUhler = useRaskuttiUhler;
+    }
+
+    /**
+     * Sets whether to use data order for GRaSP (as opposed to random order) for the first step of GRaSP
+     *
+     * @param useDataOrder True, if so.
+     */
+    public void setUseDataOrder(boolean useDataOrder) {
+        this.useDataOrder = useDataOrder;
+    }
+
+    /**
+     * Sets whether to use score for GRaSP (as opposed to independence test) for GRaSP.
+     *
+     * @param useScore True, if so.
+     */
+    public void setUseScore(boolean useScore) {
+        this.useScore = useScore;
+    }
+
+    /**
+     * Sets whether to use the discriminating path rule for GRaSP.
+     *
+     * @param doDiscriminatingPathRule True, if so.
+     */
+    public void setDoDiscriminatingPathRule(boolean doDiscriminatingPathRule) {
+        this.doDiscriminatingPathRule = doDiscriminatingPathRule;
+    }
+
+    /**
+     * Sets depth for singular tucks.
+     *
+     * @param uncoveredDepth The depth for singular tucks.
+     */
+    public void setSingularDepth(int uncoveredDepth) {
+        if (uncoveredDepth < -1) throw new IllegalArgumentException("Uncovered depth should be >= -1.");
+        this.uncoveredDepth = uncoveredDepth;
+    }
+
+    /**
+     * Sets depth for non-singular tucks.
+     *
+     * @param nonSingularDepth The depth for non-singular tucks.
+     */
+    public void setNonSingularDepth(int nonSingularDepth) {
+        if (nonSingularDepth < -1) throw new IllegalArgumentException("Non-singular depth should be >= -1.");
+        this.nonSingularDepth = nonSingularDepth;
+    }
+
+    /**
+     * Sets whether to use the ordered version of GRaSP.
+     *
+     * @param ordered True, if so.
+     */
+    public void setOrdered(boolean ordered) {
+        this.ordered = ordered;
+    }
+
+    /**
+     * <p>Setter for the field <code>seed</code>.</p>
+     *
+     * @param seed a long
+     */
+    public void setSeed(long seed) {
+        this.seed = seed;
+    }
+}
