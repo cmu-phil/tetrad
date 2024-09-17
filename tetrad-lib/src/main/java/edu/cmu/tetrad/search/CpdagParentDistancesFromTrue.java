7cfe0e3b67f118c8a43a1ef2d9d3029a8597d7b2
jdramsey <jdramsey@andrew.cmu.edu>
2024-09-17 16:19:30 -0400

    Refactor distance calculation logic and add unit test.
    Refactor distance calculation logic in CpdagParentDistancesFromTrue to improve clarity and performance. Added validation for matching node names in the CPDAG and DataSet. Introduced a new test class, TestCpdagParentDistancesFromTrue, with a test case for verifying the distance calculation method.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/search/CpdagParentDistancesFromTrue.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/CpdagParentDistancesFromTrue.java
@@ -7,6 +7,7 @@ import edu.cmu.tetrad.graph.Graph;
 import edu.cmu.tetrad.graph.Node;
 import edu.cmu.tetrad.regression.RegressionDataset;
 import edu.cmu.tetrad.regression.RegressionResult;
+import edu.cmu.tetrad.util.Matrix;
 import edu.cmu.tetrad.util.SublistGenerator;
 
 import java.util.ArrayList;
@@ -43,8 +44,6 @@ public class CpdagParentDistancesFromTrue {
      * The distance matrix is a square matrix where the entry at row v and column u is the distance for the edge u -&gt;
      * v.
      *
-     * @param nodes             The full list of nodes; these will be used to index the true edge strengths and the
-     *                          distance matrix.
      * @param outputCpdag       The estimated CPDAG.
      * @param trueEdgeStrengths The true edge strengths (coefficients) for the true DAG, where trueEdgeStrengths[u][v]
      *                          is the beta coefficient for u -&gt; v.
@@ -53,97 +52,141 @@ public class CpdagParentDistancesFromTrue {
      * @return A matrix of distances between true edge strengths and estimated strengths. Here, dist[u][v] is the
      * distance for the edge u -&gt; v.
      */
-    public static double[][] getDistances(List<Node> nodes, Graph outputCpdag, double[][] trueEdgeStrengths, DataSet dataSet,
+    public static double[][] getDistances(Graph outputCpdag, double[][] trueEdgeStrengths, DataSet dataSet,
                                           DistanceType distanceType) {
         int n = outputCpdag.getNumNodes(); // Number of nodes in the graph
-        double[][] dist = new double[n][n]; // Initialize the distance matrix
 
         // Get the list of nodes in the outputCpdag
-        List<Node> _nodes = outputCpdag.getNodes();
+        List<Node> nodes = outputCpdag.getNodes();
 
-        // Make sure the nodes in the output CPDAG are all contained the given list of nodes
-        if (!new HashSet<>(nodes).containsAll(_nodes)) {
-            throw new IllegalArgumentException("The list of nodes in the CPDAG must be a subset of the list of nodes.");
+        // Make sure the nodes in the CPDAG and the data set match name-wise.
+        List<String> names1 = outputCpdag.getNodeNames();
+        List<String> names2 = dataSet.getVariableNames();
+
+        if (!names1.equals(names2)) {
+            throw new IllegalArgumentException("The nodes in the CPDAG and the data set must match name-wise.");
         }
 
-        // Parallelize the outer loop over u
-        nodes.parallelStream().forEach(nodeU -> {
-            int u = nodes.indexOf(nodeU); // Get the index of u
-            // Iterate over each vertex v (by index)
-            for (int v = 0; v < n; v++) {
-                // Calculate the distance for the edge u -> v
-                dist[u][v] = calculateDistanceForEdge(u, v, outputCpdag, trueEdgeStrengths, dataSet, nodes, distanceType);
-            }
-        });
+        // Initialize the min and max coefficient matrices
+        double[][] minCoef = new double[n][n];
+        double[][] maxCoef = new double[n][n];
 
-        return dist; // Return the distance matrix
+        // Initialize all values in minCoef to Double.POSITIVE_INFINITY and all values in maxCoef to
+        // Double.NEGATIVE_INFINITY
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                minCoef[i][j] = Double.POSITIVE_INFINITY;
+                maxCoef[i][j] = Double.NEGATIVE_INFINITY;
+            }
+        }
+
+        RegressionDataset regressionDataset = new RegressionDataset(dataSet);
+
+        // Iterate over each node v
+        for (int v = 0; v < n; v++) {
+            calculateMinMaxCoefPerNode(v, outputCpdag, minCoef, maxCoef, nodes, regressionDataset);
+        }
+
+//        System.out.println("min = " + new Matrix(minCoef));
+//        System.out.println("max = " + new Matrix(maxCoef));
+
+        // Initialize the distance matrix
+        double[][] distances = new double[n][n];
+
+        // Calculate the distances.
+        for (int u = 0; u < n; u++) {
+            for (int v = 0; v < n; v++) {
+                distances[u][v] = calculateDistancesForEdge(u, v, trueEdgeStrengths, minCoef, maxCoef, distanceType);
+            }
+        }
+
+//        System.out.println("distances = " + new Matrix(distances));
+
+        return distances;
     }
 
     /**
-     * Calculates the distance for a specific edge u -&gt; v. The distance is based on the difference between the true
-     * edge strength to the range of estimated regression coefficients. The range is determined by considering all
-     * possible parent sets for node v. The distance is 0 if the true edge strength falls within the range of
-     * coefficients. If the true edge strength is less than the minimum coefficient, the distance is the absolute value
-     * of the difference between the true strength and the minimum coefficient. If the true edge strength is greater
-     * than the maximum coefficient, the distance is the absolute value of the difference between the true strength and
-     * the maximum coefficient.
-     *
-     * @param u                 Index of the parent node u.
-     * @param v                 Index of the child node v.
-     * @param outputCpdag       The estimated CPDAG.
-     * @param trueEdgeStrengths The true edge strengths (coefficients). Here, trueEdgeStrengths[u][v] is the beta
-     *                          coefficient for u -&gt; v.
-     * @param dataSet           The dataset used for regression.
-     * @param nodes             List of nodes in the CPDAG.
-     * @param distanceType      The type of distance to calculate (absolute or squared).
-     * @return The distance between the true edge strength and the range of possible estimated coefficients.
+     * Adjusts the min and max estimated coefficients for nodes u adjacent to v in the CPDAG by regressing node v on all
+     * possible parents set of v that do not imply new unshielded colliders. The adjusted min and max coefficients are
+     * stored in the minCoef and maxCoef matrices.
      */
-    private static double calculateDistanceForEdge(int u, int v, Graph outputCpdag, double[][] trueEdgeStrengths,
-                                                   DataSet dataSet, List<Node> nodes, DistanceType distanceType) {
-        List<Double> coefflist = new ArrayList<>(); // List to hold regression coefficients
-        RegressionDataset regressionDataset = new RegressionDataset(dataSet); // Regression dataset wrapper
+    private static void calculateMinMaxCoefPerNode(int v, Graph outputCpdag,
+                                                   double[][] minCoef, double[][] maxCoef, List<Node> nodes,
+                                                   RegressionDataset regressionDataSet) {
 
         // Form all possible parent sets of v
         List<List<Node>> possibleParentSets = formAllPossibleParentSets(nodes.get(v), outputCpdag);
 
+        List<Node> adj = outputCpdag.getAdjacentNodes(nodes.get(v));
+
         // Iterate over each parent set
         for (List<Node> parentList : possibleParentSets) {
+
             // Regress v on the parent set
-            RegressionResult regResult = regress(nodes.get(v), parentList, regressionDataset);
+            RegressionResult regResult = regress(nodes.get(v), parentList, regressionDataSet);
             double[] regCoeffs = regResult.getCoef(); // Get the regression coefficients
 
-            // Add the coefficient for each parent node
-            for (Node parent : parentList) {
-                coefflist.add(regCoeffs[parentList.indexOf(parent)] + 1); // The coef at index 0 is the intercept.
+            // Update the min and max coefficients for each edge u -> v
+            for (int u = 0; u < parentList.size(); u++) {
+                int _u = nodes.indexOf(parentList.get(u)); // Get the index of u
+
+                double regCoeff = regCoeffs[u + 1]; // Get the regression coefficient for u -> v; skip the intercept
+
+                // Update the min and max coefficients for u -> v
+                if (regCoeff < minCoef[_u][v]) {
+                    minCoef[_u][v] = regCoeff;
+                }
+
+                if (regCoeff > maxCoef[_u][v]) {
+                    maxCoef[_u][v] = regCoeff;
+                }
             }
 
-            // For nodes not in the parent set, add 0 as the coefficient
-            for (Node other : outputCpdag.getNodes()) {
-                if (!parentList.contains(other)) {
-                    coefflist.add(0.0);
+            // Adjust min and max for the value 0 for all nodes u adjacent to v but not in the parent set
+            List<Node> compl = new ArrayList<>(adj);
+            compl.removeAll(parentList);
+
+            for (Node u : compl) {
+                int _u = nodes.indexOf(u); // Get the index of u
+
+                // Update the min and max coefficients for u -> v
+                if (0 < minCoef[_u][v]) {
+                    minCoef[_u][v] = 0;
+                }
+
+                if (0 > maxCoef[_u][v]) {
+                    maxCoef[_u][v] = 0;
                 }
             }
         }
+    }
+
+    private static double calculateDistancesForEdge(int u, int v, double[][] trueEdgeStrengths,
+                                                    double[][] minCoef, double[][] maxCoef, DistanceType distanceType) {
 
         // Get the true edge strength for u -> v
         double trueStrength = trueEdgeStrengths[u][v];
-        double minCoeff = coefflist.stream().min(Double::compare).orElse(trueStrength);
-        double maxCoeff = coefflist.stream().max(Double::compare).orElse(trueStrength);
+
+        // Get the min estimated coef
+        double min = minCoef[u][v] == Double.POSITIVE_INFINITY ? 0.0 : minCoef[u][v];
+
+        // Get the max estimated coef
+        double max = maxCoef[u][v] == Double.NEGATIVE_INFINITY ? 0.0 : maxCoef[u][v];
 
         // Calculate the distance based on whether true strength falls within the range of coefficients
-        if (minCoeff < trueStrength && trueStrength < maxCoeff) {
+        if (min < trueStrength && trueStrength < max) {
             return 0.0; // No distance if true strength is within the coefficient range
-        } else if (trueStrength <= minCoeff) {
+        } else if (trueStrength <= min) {
             if (distanceType == DistanceType.SQUARED) {
-                return Math.pow(trueStrength - minCoeff, 2); // Squared distance from the minimum coefficient
+                return Math.pow(trueStrength - min, 2); // Squared distance from the minimum coefficient
             } else {
-                return Math.abs(trueStrength - minCoeff); // Distance from the minimum coefficient
+                return Math.abs(trueStrength - min); // Distance from the minimum coefficient
             }
         } else {
             if (distanceType == DistanceType.SQUARED) {
-                return Math.pow(trueStrength - maxCoeff, 2); // Squared distance from the maximum coefficient
+                return Math.pow(trueStrength - max, 2); // Squared distance from the maximum coefficient
             } else {
-                return Math.abs(trueStrength - maxCoeff); // Distance from the maximum coefficient
+                return Math.abs(trueStrength - max); // Distance from the maximum coefficient
             }
         }
     }
@@ -171,29 +214,46 @@ public class CpdagParentDistancesFromTrue {
             }
         }
 
-        List<List<Node>> combinations = new ArrayList<>(); // List of all parent set combinations
+        // Make a list to hold all possible parent sets that do not imply new unshielded colliders.
+        List<List<Node>> combinations = new ArrayList<>();
 
         // Iterate over all combinations of undirected edges
         SublistGenerator sublistGenerator = new SublistGenerator(undirectedEdges.size(), undirectedEdges.size());
         int[] choice;
 
+        CHOICE:
         while ((choice = sublistGenerator.next()) != null) {
-            List<Node> combination = new ArrayList<>();
+            List<Node> parentNodes = new ArrayList<>();
 
             // Add all parent edges to the combination
             for (Edge edge : parentEdges) {
-                combination.add(edge.getDistalNode(v));
+                parentNodes.add(edge.getDistalNode(v));
             }
 
-            // Add selected undirected edges to the combination
+            // Get the set of undirected neighbors.
+            Set<Node> undirectedNeighbors = new HashSet<>();
             for (int i : choice) {
-                combination.add(undirectedEdges.get(i).getDistalNode(v));
+                undirectedNeighbors.add(undirectedEdges.get(i).getDistalNode(v));
             }
 
-            combinations.add(combination); // Add the combination to the list
+            // Add the undirected neighbors to the parent set one at a time, checking at each step to make sure each
+            // newly added node doesn't form a new unshielded collider with any other nodes in the parent set.
+            for (Node z : undirectedNeighbors) {
+                for (Node x : parentNodes) {
+                    if (z != x && !cpdag.isAdjacentTo(x, z)) {
+                        continue CHOICE;
+                    }
+                }
+
+                parentNodes.add(z);
+            }
+
+            combinations.add(parentNodes);
         }
 
-        return combinations; // Return all possible parent sets
+//        System.out.println("Parent sets for node " + v + ": " + combinations);
+
+        return combinations;
     }
 
     /**
