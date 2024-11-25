881adba88d8ef65ec9a531c1a462c33db08fdbd4
jdramsey <jdramsey@andrew.cmu.edu>
2024-11-25 04:06:20 -0500

    Refactor and parallelize ConditionalCorrelationIndependence
    Refactor the ConditionalCorrelationIndependence class to use modern Java features, including ConcurrentHashMap and IntStream for parallel processing. Streamline the code by removing redundant comments and improving method documentation.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/search/test/ConditionalCorrelationIndependence.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/test/ConditionalCorrelationIndependence.java
@@ -29,6 +29,8 @@ import edu.cmu.tetrad.util.Vector;
 import org.apache.commons.math3.distribution.NormalDistribution;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.IntStream;
 
 import static edu.cmu.tetrad.util.StatUtils.correlation;
 import static edu.cmu.tetrad.util.StatUtils.getZForAlpha;
@@ -55,89 +57,61 @@ import static org.apache.commons.math3.util.FastMath.*;
  * @author josephramsey
  */
 public final class ConditionalCorrelationIndependence {
-    /**
-     * The variables in datasSet.
-     */
-    private final List<Node> variables;
-    /**
-     * Map from nodes to their indices.
-     */
-    private final HashMap<Node, Integer> nodesHash;
-    /**
-     * The data matrix representing the correlation data implied by the given data set. It is used in the independence
-     * test calculations.
-     */
+    private final Map<Node, Integer> nodesHash;
     private final Matrix data;
-    /**
-     * The q value of the most recent test.
-     */
     private double score;
-    /**
-     * Number of functions to use in the (truncated) basis.
-     */
     private int numFunctions = 10;
-    /**
-     * Z cutoff for testing; depends on alpha.
-     */
     private double cutoff;
-    /**
-     * Bandwidth.
-     */
     private double bandwidth = 1.0;
 
     /**
-     * Constructs a new Independence test which checks independence facts based on the correlation data implied by the
-     * given data set (must be continuous). The given significance level is used.
+     * Initializes a new instance of the ConditionalCorrelationIndependence class using the provided DataSet.
      *
-     * @param dataSet A data set containing only continuous columns.
+     * @param dataSet The dataset to be used for the analysis. This dataset must not be null and will be standardized.
+     * @throws NullPointerException if the provided dataset is null.
      */
     public ConditionalCorrelationIndependence(DataSet dataSet) {
         if (dataSet == null) throw new NullPointerException();
         dataSet = DataTransforms.standardizeData(dataSet);
         data = dataSet.getDoubleData();
 
-        this.variables = dataSet.getVariables();
-        this.nodesHash = new HashMap<>();
+        this.nodesHash = new ConcurrentHashMap<>();
 
-        for (int i = 0; i < this.variables.size(); i++) {
-            this.nodesHash.put(this.variables.get(i), i);
+        for (int i = 0; i < dataSet.getVariables().size(); i++) {
+            this.nodesHash.put(dataSet.getVariables().get(i), i);
         }
     }
 
     /**
-     * Computes the Gaussian kernel value between two vectors given a specific bandwidth.
+     * Computes the Gaussian kernel value between two rows in a given matrix.
      *
-     * @param z         The data we're looking for the Gaussian kernal of.
-     * @param i         The index of the first vector.
-     * @param j         The index of the second vector.
+     * @param z         The matrix containing the data points.
+     * @param i         The index of the first row.
+     * @param j         The index of the second row.
      * @param bandwidth The bandwidth parameter for the Gaussian kernel.
-     * @return The computed Gaussian kernel value.
+     * @return The computed Gaussian kernel value between the two rows.
      */
     private static double gaussianKernel(Matrix z, int i, int j, double bandwidth) {
-        double squaredDistance = 0.0;
-        for (int k = 0; k < z.getNumColumns(); k++) {
-            double diff = z.get(i, k) - z.get(j, k);
-            squaredDistance += diff * diff;
-        }
+        double squaredDistance = IntStream.range(0, z.getNumColumns())
+                .mapToDouble(k -> {
+                    double diff = z.get(i, k) - z.get(j, k);
+                    return diff * diff;
+                }).sum();
         return Math.exp(-squaredDistance / (2 * bandwidth * bandwidth));
     }
 
     /**
-     * Returns the p-value of the test, x _||_ y | z. Can be compared to alpha.
+     * Determines whether two given nodes are independent given a set of conditioning nodes, and calculates a score.
      *
-     * @param x  a {@link edu.cmu.tetrad.graph.Node} object
-     * @param y  a {@link edu.cmu.tetrad.graph.Node} object
-     * @param _z a {@link java.util.Set} object
-     * @return This p-value.
+     * @param x  The first node.
+     * @param y  The second node.
+     * @param _z The set of conditioning nodes.
+     * @return The score representing the level of independence between nodes x and y given the conditioning set _z.
+     * Returns Double.NaN if the score cannot be computed or is not a number.
      */
     public double isIndependent(Node x, Node y, Set<Node> _z) {
         List<Node> z = new ArrayList<>(_z);
         Collections.sort(z);
-
-        Map<Node, Integer> nodesHash = new HashMap<>();
-        for (int i = 0; i < this.variables.size(); i++) {
-            nodesHash.put(this.variables.get(i), i);
-        }
 
         List<Node> allNodes = new ArrayList<>(z);
         allNodes.add(x);
@@ -152,7 +126,6 @@ public final class ConditionalCorrelationIndependence {
         Vector rx = residuals(x, z);
         Vector ry = residuals(y, z);
 
-        // rx _||_ ry ?
         double score = independent(rx, ry);
         this.score = score;
 
@@ -164,18 +137,18 @@ public final class ConditionalCorrelationIndependence {
     }
 
     /**
-     * Computes the residuals of a kernel regression for the given data points.
+     * Calculates the residuals of a kernel regression using the Gaussian kernel.
      *
-     * @param x         The array of response variable values.
-     * @param z         The array of predictor variable vectors.
+     * @param x         The vector containing the response variable data points.
+     * @param z         The matrix containing the predictor variable data points.
      * @param bandwidth The bandwidth parameter for the Gaussian kernel.
-     * @return An array of residuals from the kernel regression.
+     * @return A vector containing the residuals from the kernel regression for each data point.
      */
     private Vector kernelRegressionResiduals(Vector x, Matrix z, double bandwidth) {
         int n = x.size();
         Vector residuals = new Vector(n);
 
-        for (int i = 0; i < n; i++) {
+        IntStream.range(0, n).parallel().forEach(i -> {
             double weightSum = 0.0;
             double weightedXSum = 0.0;
 
@@ -187,17 +160,17 @@ public final class ConditionalCorrelationIndependence {
 
             double fittedValue = weightedXSum / weightSum;
             residuals.set(i, x.get(i) - fittedValue);
-        }
+        });
 
         return residuals;
     }
 
     /**
-     * Computes the residuals from a kernel regression of the response variable on the predictor variables.
+     * Computes the residuals of a node x given a list of conditioning nodes z using kernel regression.
      *
-     * @param x The response variable represented as a Node.
-     * @param z A list of predictor variables represented as Nodes.
-     * @return An array of residuals from the kernel regression.
+     * @param x The node for which residuals need to be computed.
+     * @param z The list of conditioning nodes.
+     * @return A vector containing the residuals of node x after accounting for the conditioning nodes z.
      */
     private Vector residuals(Node x, List<Node> z) {
         Vector _x = data.getColumn(nodesHash.get(x));
@@ -212,68 +185,73 @@ public final class ConditionalCorrelationIndependence {
     }
 
     /**
-     * Sets the number of functions to use in (truncated) basis
+     * Sets the number of functions utilized in the ConditionalCorrelationIndependence analysis.
      *
-     * @param numFunctions This number.
+     * @param numFunctions the number of functions to set. This value must be a positive integer.
      */
     public void setNumFunctions(int numFunctions) {
         this.numFunctions = numFunctions;
     }
 
     /**
-     * Returns the kernel width.
+     * Retrieves the current bandwidth parameter used in the ConditionalCorrelationIndependence analysis.
      *
-     * @return This width.
+     * @return The current value of the bandwidth parameter.
      */
     public double getBandwidth() {
         return this.bandwidth;
     }
 
     /**
-     * Sets the kernel width.
+     * Sets the bandwidth parameter for the ConditionalCorrelationIndependence analysis.
      *
-     * @param bandwidth This width.
+     * @param bandwidth The bandwidth parameter to be set. This value is used in the Gaussian kernel calculation for
+     *                  various methods in the analysis.
      */
     public void setBandwidth(double bandwidth) {
         this.bandwidth = bandwidth;
     }
 
     /**
-     * Returns the p-value of the score.
+     * Calculates the p-value for a given score using the cumulative distribution function (CDF) of a standard normal
+     * distribution.
      *
-     * @param score The score.
-     * @return This p-value.
+     * @param score The score for which the p-value needs to be calculated. This score is typically a test statistic
+     *              resulting from some statistical test.
+     * @return The p-value corresponding to the given score, indicating the probability of obtaining a value at least as
+     * extreme as the observed score under the null hypothesis.
      */
     public double getPValue(double score) {
         return 2.0 * (1.0 - new NormalDistribution(0, 1).cumulativeProbability(abs(score)));
     }
 
     /**
-     * Returns the minimal scores value calculated by the method for the most recent independence check, less the cutoff
-     * so that negative scores correspond to independence.
+     * Retrieves the score modified by applying an absolute value and subtracting a cutoff value.
      *
-     * @return This minimal score.
+     * @return The modified score which is calculated as the absolute value of the current score minus the cutoff value.
      */
     public double getScore() {
         return abs(this.score) - this.cutoff;
     }
 
     /**
-     * Sets the alpha cutoff.
+     * Sets the alpha value and updates the cutoff accordingly.
      *
-     * @param alpha This cutoff.
+     * @param alpha The alpha value to be set. This is used to determine the cutoff for statistical testing and
+     *              typically represents the significance level.
      */
     public void setAlpha(double alpha) {
         this.cutoff = getZForAlpha(alpha);
     }
 
     /**
-     * Determines the maximum absolute value of the nonparametric Fisher Z test statistic between transformed versions
-     * of the given arrays `x` and `y` across different functions.
+     * Computes the maximum absolute non-parametric Fisher's Z value between two vectors, x and y, based on a range of
+     * functions applied to the elements of these vectors.
      *
-     * @param x The first array of input values.
-     * @param y The second array of input values.
-     * @return The maximum absolute value of the nonparametric Fisher Z test statistic.
+     * @param x The first vector containing data points.
+     * @param y The second vector containing data points.
+     * @return The maximum absolute value of the non-parametric Fisher's Z calculated between the transformed versions
+     * of vectors x and y.
      */
     private double independent(Vector x, Vector y) {
         Vector _x = new Vector(x.size());
@@ -282,8 +260,6 @@ public final class ConditionalCorrelationIndependence {
         double max = 0.0;
 
         for (int m = 0; m <= this.numFunctions; m++) {
-
-            N:
             for (int n = 0; n <= this.numFunctions; n++) {
                 for (int i = 0; i < x.size(); i++) {
                     double fx = function(m, x.get(i));
@@ -291,7 +267,7 @@ public final class ConditionalCorrelationIndependence {
 
                     if (Double.isInfinite(fx) || Double.isInfinite(fy)
                         || Double.isNaN(fx) || Double.isNaN(fy)) {
-                        continue N;
+                        continue;
                     }
 
                     _x.set(i, fx);
@@ -312,11 +288,11 @@ public final class ConditionalCorrelationIndependence {
     }
 
     /**
-     * Calculates the nonparametric Fisher Z test for the given arrays.
+     * Computes the non-parametric Fisher's Z value for two vectors.
      *
-     * @param _x An array of double values.
-     * @param _y An array of double values.
-     * @return The nonparametric Fisher Z test statistic.
+     * @param _x The first vector containing data points.
+     * @param _y The second vector containing data points.
+     * @return The non-parametric Fisher's Z value between the two vectors.
      */
     private double nonparametricFisherZ(Vector _x, Vector _y) {
         double r = correlation(_x, _y);
@@ -325,28 +301,25 @@ public final class ConditionalCorrelationIndependence {
     }
 
     /**
-     * Calculates the moment22 value for the given arrays x and y.
+     * Computes a normalized sum of the element-wise product of the squares of two vectors.
      *
-     * @param x An array of double values.
-     * @param y An array of double values.
-     * @return The moment22 value.
+     * @param x The first vector containing data points.
+     * @param y The second vector containing data points.
+     * @return The normalized sum of the element-wise product of the squares of the two vectors.
      */
     private double m22(Vector x, Vector y) {
-        double sum = 0.0;
-
-        for (int i = 0; i < x.size(); i++) {
-            sum += x.get(i) * x.get(i) * y.get(i) * y.get(i);
-        }
-
-        return sum / x.size();
+        return IntStream.range(0, x.size())
+                       .mapToDouble(i -> x.get(i) * x.get(i) * y.get(i) * y.get(i))
+                       .sum() / x.size();
     }
 
     /**
-     * Calculates a basis function value based on the given index and input value.
+     * Performs a calculation that involves repeatedly multiplying an initial value of `1.0` by the product of `0.95`
+     * and a given parameter `x`, iterating `index` times.
      *
-     * @param index The index of the basis function.
-     * @param x     The input value.
-     * @return The basis function value.
+     * @param index The number of iterations to perform the multiplication.
+     * @param x     The value to be multiplied by `0.95` in each iteration.
+     * @return The result of the iterative multiplication.
      */
     private double function(int index, double x) {
         double g = 1.0;
@@ -359,32 +332,29 @@ public final class ConditionalCorrelationIndependence {
     }
 
     /**
-     * Retrieves the list of row indices in the dataSet that have no NaN values for all variables in allVars.
+     * Retrieves the list of row indices from a given matrix that do not contain NaN values for any specified nodes.
      *
-     * @param data      The data matrix.
-     * @param allVars   The list of variables to check for NaN values.
-     * @param nodesHash The map containing the mapping of nodes to their corresponding column indices in the dataSet.
-     * @return The list of row indices that have no NaN values for all variables in allVars.
+     * @param data      The matrix containing the data.
+     * @param allVars   The list of nodes to be checked.
+     * @param nodesHash A map associating each node with its respective column index in the matrix.
+     * @return A list of row indices where none of the specified nodes' values are NaN in the matrix.
      */
     private List<Integer> getRows(Matrix data, List<Node> allVars, Map<Node, Integer> nodesHash) {
         List<Integer> rows = new ArrayList<>();
 
-        K:
         for (int k = 0; k < data.getNumRows(); k++) {
+            boolean hasNaN = false;
             for (Node node : allVars) {
-
-                // Testwise deletion.
-                if (Double.isNaN(data.get(k, nodesHash.get(node)))) continue K;
+                if (Double.isNaN(data.get(k, nodesHash.get(node)))) {
+                    hasNaN = true;
+                    break;
+                }
             }
-
-            rows.add(k);
+            if (!hasNaN) {
+                rows.add(k);
+            }
         }
 
         return rows;
     }
 }
-
-
-
-
-
