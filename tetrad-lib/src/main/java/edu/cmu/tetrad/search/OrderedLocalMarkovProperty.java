481ce987c4a40c950b57e939333aab47b516645c
Bryan Andrews <bja43@pitt.edu>
2025-02-17 20:42:32 -0600

    made olmp

---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/OrderedLocalMarkovProperty.java
@@ -0,0 +1,55 @@
+package edu.cmu.tetrad.search;
+
+import edu.cmu.tetrad.graph.*;
+
+import java.util.*;
+
+public class OrderedLocalMarkovProperty {
+
+    private OrderedLocalMarkovProperty() {}
+
+    public static Set<IndependenceFact> getModel(Graph mag) {
+        // there should probably be a check to ensure the input is a mag
+        Set<IndependenceFact> model = new HashSet<>();
+
+        Paths paths = new Paths(mag);
+        Map<Node, Set<Node>> de = paths.getDescendantsMap();
+
+        List<Node> unprocessed = new ArrayList<>(mag.getNodes());
+        EdgeListGraph mag_ = new EdgeListGraph((mag));
+
+        while (!unprocessed.isEmpty()) {
+            Node sink = unprocessed.getFirst();
+            while (!mag_.getChildren(sink).isEmpty()) {
+                sink = mag_.getChildren(sink).getFirst();
+            }
+
+            Set<Node> dis = GraphUtils.district(sink, mag_);
+            processSink(model, de, sink, dis, mag_);
+            mag_.removeNode(sink);
+        }
+
+        return model;
+    }
+
+    private static void processSink(Set<IndependenceFact> model, Map<Node, Set<Node>> de,
+                                    Node sink, Set<Node> dis, EdgeListGraph mag) {
+        Set<Node> mb = GraphUtils.markovBlanket(sink, mag);
+
+        for (Node node : mag.getNodes()) {
+            if (node == sink) continue;
+            if (mb.contains(node)) continue;
+            model.add(new IndependenceFact(sink, node, mb));
+        }
+
+        for (Node node : dis) {
+            Set<Node> dis_ = new HashSet<>(dis);
+            dis_.remove((node));
+
+            EdgeListGraph mag_ = new EdgeListGraph(mag);
+            mag_.removeNodes(new ArrayList<>(de.get(node)));
+
+            processSink(model, de, sink, dis_, mag_);
+        }
+    }
+}
