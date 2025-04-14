c10645c8f75c75b22d9da0bad1f2866d0758ebf5
jdramsey <jdramsey@andrew.cmu.edu>
2025-04-14 03:35:54 -0400

    Add Javadoc for DataSetHelper and its fromR method
    Introduce comprehensive documentation for the DataSetHelper class and its fromR method. This ensures clarity on its purpose, parameters, and return type, improving code maintainability and usability.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/util/DataSetHelper.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/util/DataSetHelper.java
@@ -7,7 +7,21 @@ import edu.cmu.tetrad.graph.Node;
 
 import java.util.List;
 
+/**
+ * Utility class for creating a {@link DataSet} instance from various inputs.
+ */
 public class DataSetHelper {
+
+    /**
+     * Creates a new {@link DataSet} instance from the specified inputs, including
+     * continuous and discrete data along with variable definitions.
+     *
+     * @param vars the list of variables, represented as {@link Node} objects
+     * @param nrows the number of rows in the resulting dataset
+     * @param cont a 2D array of continuous data values
+     * @param disc a 2D array of discrete data values
+     * @return a {@link DataSet} instance containing the specified data
+     */
     public static DataSet fromR(List<Node> vars, int nrows, double[][] cont, int[][] disc) {
         MixedDataBox box = new MixedDataBox(vars, nrows, cont, disc);
         return new BoxDataSet(box, vars);
