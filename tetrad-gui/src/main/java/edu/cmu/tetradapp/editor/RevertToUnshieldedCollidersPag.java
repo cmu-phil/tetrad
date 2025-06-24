3815b61c7fd535a9d42be6e44edec982b88b4acd
jdramsey <jdramsey@andrew.cmu.edu>
2025-06-24 11:19:13 -0400

    Add unshielded colliders functionality in PAG and CPDAG workflows, enhance edge flipping logic in `Edge`, and improve recursive blocking methods.

--- tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/RevertToUnshieldedCollidersPag.java
+++ tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/RevertToUnshieldedCollidersPag.java
@@ -72,7 +72,7 @@ public class RevertToUnshieldedCollidersPag extends AbstractAction implements Cl
             return;
         }
 
-        Graph _graph = GraphUtils2.revertToUnshieldedCollidersPag(graph);
+        Graph _graph = GraphUtils.revertUnshieldedPag(graph);
 
         workbench.setGraph(_graph);
     }
