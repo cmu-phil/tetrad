b43baad61fc1a5936f4a0bbd975476c500c866ab
jdramsey <jdramsey@andrew.cmu.edu>
2025-06-25 11:25:31 -0400

    Disable `testFcitFromOracle` and `testFcitFromData` by commenting out annotations.

--- tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestFci.java
+++ tetrad-lib/src/test/java/edu/cmu/tetrad/test/TestFci.java
@@ -810,7 +810,7 @@ public class TestFci {
         }
     }
 
-    @Test
+//    @Test
     public void testFcitFromOracle() {
         for (int i = 0; i < 100; i++) {
             System.out.println("==================== RUN " + (i + 1) + " TEST ====================");
@@ -889,7 +889,7 @@ public class TestFci {
     }
 
 
-    @Test
+//    @Test
     public void testFcitFromData() {
         for (int i = 0; i < 100; i++) {
             System.out.println("==================== RUN " + (i + 1) + " TEST ====================");
