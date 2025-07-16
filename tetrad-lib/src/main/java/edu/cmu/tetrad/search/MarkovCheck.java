e850c484185bef37c670c696a050145c8d18bb98
vbcwonderland <veritybingchu@cmu.edu>
2025-07-15 23:03:55 -0400

    getF1StatsForTargetNodeParents

--- tetrad-lib/src/main/java/edu/cmu/tetrad/search/MarkovCheck.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/search/MarkovCheck.java
@@ -695,6 +695,22 @@ public class MarkovCheck implements EffectiveSampleSizeSettable {
         return Arrays.asList(f1Adj, f1Arrow, f1Circle, f1Tail);
     }
 
+    public List<Double> getF1StatsForTargetNodeParents(Node x, Graph estimatedGraph, Graph trueGraph) {
+        Graph lookupGraph = GraphUtils.replaceNodes(trueGraph, estimatedGraph.getNodes());
+        Graph xParentsLookupGraph = GraphUtils.getParentsSubgraphWithTargetNode(lookupGraph, x);
+        Graph xParentsEstimatedGraph = GraphUtils.getParentsSubgraphWithTargetNode(estimatedGraph, x);
+
+        double f1Adj = new F1Adj().getValue(xParentsLookupGraph, xParentsEstimatedGraph);
+        double f1Arrow = new F1Arrow().getValue(xParentsLookupGraph, xParentsEstimatedGraph);
+        double f1Circle = new F1Circle().getValue(xParentsLookupGraph, xParentsEstimatedGraph);
+        double f1Tail = new F1Tail().getValue(xParentsLookupGraph, xParentsEstimatedGraph);
+
+        return Arrays.asList(f1Adj, f1Arrow, f1Circle, f1Tail);
+    }
+
+
+
+
     /**
      * Calculates the precision and recall on the Markov Blanket graph for a given node. Prints the statistics to the
      * console.
