1c09e5a82232f79da7d4e56bceb530c7dd29b9ec
jdramsey <jdramsey@andrew.cmu.edu>
2025-09-01 12:12:41 -0400

    Update `RandomMim` class documentation to clarify it creates a random MIM instead of adding forward edges.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/graph/RandomMim.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/graph/RandomMim.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * Creates a random graph by adding forward edges.
+ * Creates a random MIM.
  *
  * @author josephramsey
  * @version $Id: $Id
