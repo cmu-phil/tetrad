43c8fcb680b0eca200f59a7bd4fee2ecc796d65d
jdramsey <jdramsey@andrew.cmu.edu>
2025-11-26 11:58:40 -0500

    Simplify `PathsAction` logic and refactor `adjustmentSets` in `Paths` for consistency and cleaner implementation.

--- tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/PathsAction.java
+++ tetrad-gui/src/main/java/edu/cmu/tetradapp/editor/PathsAction.java
@@ -694,7 +694,6 @@ public class PathsAction extends AbstractAction implements ClipboardOwner {
 
             if ("SELECT_ALL".equals(node.getName())) {
                 PathsAction.this.nodes2 = new ArrayList<>(graph.getNodes());
-            } else {
                 PathsAction.this.nodes2 = Collections.singletonList(node);
             }
 
