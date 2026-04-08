2d9d076316f79e8a6aa016fcc4e0a0c3726d3b2e
jdramsey <jdramsey@andrew.cmu.edu>
2026-04-08 18:24:47 -0400

    Fix indentation for the `TIME_LAG` constant in `Params` for code style consistency.

--- tetrad-lib/src/main/java/edu/cmu/tetrad/util/Params.java
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/util/Params.java
@@ -846,7 +846,7 @@ public final class Params {
     /**
      * Constant <code>TIME_LAG="timeLag"</code>
      */
-    public static final String TIME_LAG = "timeLag";
+        public static final String TIME_LAG = "timeLag";
     /**
      * Constant <code>PRECOMPUTE_COVARIANCES="precomputeCovariances"</code>
      */
