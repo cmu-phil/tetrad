099f0a3e374bfdbbe979fef893820e7bb91ced61
jdramsey <jdramsey@andrew.cmu.edu>
2026-06-21 02:58:58 -0400

    Add `FcitMarkov` class to implement a Markov-preserving variant of `Fcit` with enhanced m-separation checks, optimized constraint evaluations, and spurious edge handling.

---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/algcomparison/algorithm/oracle/pag/FcitMarkov.java
@@ -0,0 +1,305 @@
+///////////////////////////////////////////////////////////////////////////////
+// For information as to what this class does, see the Javadoc, below.       //
+//                                                                           //
+// Copyright (C) 2025 by Joseph Ramsey, Peter Spirtes, Clark Glymour,        //
+// and Richard Scheines.                                                     //
+//                                                                           //
+// This program is free software: you can redistribute it and/or modify      //
+// it under the terms of the GNU General Public License as published by      //
+// the Free Software Foundation, either version 3 of the License, or         //
+// (at your option) any later version.                                       //
+//                                                                           //
+// This program is distributed in the hope that it will be useful,           //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             //
+// GNU General Public License for more details.                              //
+//                                                                           //
+// You should have received a copy of the GNU General Public License         //
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.    //
+///////////////////////////////////////////////////////////////////////////////
+
+package edu.cmu.tetrad.algcomparison.algorithm.oracle.pag;
+
+import edu.cmu.tetrad.algcomparison.algorithm.*;
+import edu.cmu.tetrad.algcomparison.independence.IndependenceWrapper;
+import edu.cmu.tetrad.algcomparison.score.ScoreWrapper;
+import edu.cmu.tetrad.algcomparison.utils.AcceptsKnowledge;
+import edu.cmu.tetrad.algcomparison.utils.TakesIndependenceWrapper;
+import edu.cmu.tetrad.algcomparison.utils.TakesScoreWrapper;
+import edu.cmu.tetrad.annotation.AlgType;
+import edu.cmu.tetrad.annotation.Bootstrapping;
+import edu.cmu.tetrad.annotation.Experimental;
+import edu.cmu.tetrad.data.DataModel;
+import edu.cmu.tetrad.data.DataSet;
+import edu.cmu.tetrad.data.DataType;
+import edu.cmu.tetrad.data.Knowledge;
+import edu.cmu.tetrad.graph.Graph;
+import edu.cmu.tetrad.graph.GraphTransforms;
+import edu.cmu.tetrad.search.score.Score;
+import edu.cmu.tetrad.search.test.CachedIndependenceQueries;
+import edu.cmu.tetrad.search.test.IndependenceTest;
+import edu.cmu.tetrad.search.test.MsepTest;
+import edu.cmu.tetrad.search.utils.TsUtils;
+import edu.cmu.tetrad.util.Parameters;
+import edu.cmu.tetrad.util.Params;
+
+import java.io.Serial;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * This class represents the FCI Targeted Testing (FCIT) algorithm, which is variant of the *-FCI algorithm for learning
+ * causal structures from observational data using the BOSS algorithm as an initial CPDAG and using all score-based
+ * steps afterward.
+ *
+ * @author josephramsey
+ */
+@edu.cmu.tetrad.annotation.Algorithm(
+        name = "FCIT-Markov",
+        command = "FCIT-Markov",
+        algoType = AlgType.allow_latent_common_causes
+)
+@Bootstrapping
+@Experimental
+public class FcitMarkov extends AbstractBootstrapAlgorithm implements Algorithm, TakesScoreWrapper, TakesIndependenceWrapper,
+        AcceptsKnowledge, ReturnsBootstrapGraphs, TakesCovarianceMatrix, LatentStructureAlgorithm {
+
+    @Serial
+    private static final long serialVersionUID = 23L;
+
+    /**
+     * The independence test to use.
+     */
+    private IndependenceWrapper test;
+
+    /**
+     * The score to use.
+     */
+    private ScoreWrapper score;
+
+    /**
+     * The knowledge.
+     */
+    private Knowledge knowledge = new Knowledge();
+
+    /**
+     * This class represents a FCIT algorithm.
+     *
+     * <p>
+     * The FCIT algorithm is a bootstrap algorithm that runs a search algorithm to find a graph structure based on a
+     * given data set and parameters. It is a subclass of the Abstract BootstrapAlgorithm class and implements the
+     * Algorithm interface.
+     * </p>
+     *
+     * @see AbstractBootstrapAlgorithm
+     * @see Algorithm
+     */
+    public FcitMarkov() {
+        // Used for reflection; do not delete.
+    }
+
+    /**
+     * FCIT is a class that represents a FCIT algorithm.
+     *
+     * <p>
+     * The FCIT algorithm is a bootstrap algorithm that runs a search algorithm to find a graph structure based on a
+     * given data set and parameters. It is a subclass of the AbstractBootstrapAlgorithm class and implements the
+     * Algorithm interface.
+     * </p>
+     *
+     * @param test  The independence test to use.
+     * @param score The score to use.
+     * @see AbstractBootstrapAlgorithm
+     * @see Algorithm
+     */
+    public FcitMarkov(IndependenceWrapper test, ScoreWrapper score) {
+        this.test = test;
+        this.score = score;
+    }
+
+    /**
+     * Runs the search algorithm to find a graph structure based on a given data model and parameters.
+     *
+     * @param dataModel  The data model to use for the search algorithm.
+     * @param parameters The parameters to configure the search algorithm.
+     * @return The resulting graph structure.
+     * @throws IllegalArgumentException if the time lag is greater than 0 and the data model is not an instance of
+     *                                  DataSet.
+     */
+    @Override
+    public Graph runSearch(DataModel dataModel, Parameters parameters) throws InterruptedException {
+        if (parameters.getInt(Params.TIME_LAG) > 0) {
+            if (!(dataModel instanceof DataSet dataSet)) {
+                throw new IllegalArgumentException("Expecting a dataset for time lagging.");
+            }
+
+            DataSet timeSeries = TsUtils.createLagData(dataSet, parameters.getInt(Params.TIME_LAG), knowledge);
+            if (dataSet.getName() != null) {
+                timeSeries.setName(dataSet.getName());
+            }
+            dataModel = timeSeries;
+            knowledge = timeSeries.getKnowledge();
+        }
+
+        IndependenceTest test = this.test.getTest(dataModel, parameters);
+        Score score = this.score.getScore(dataModel, parameters);
+
+        if (test instanceof MsepTest) {
+            if (parameters.getInt(Params.FCIT_STARTS_WITH) == 1) {
+                throw new IllegalArgumentException("For d-separation oracle input, please use the GRaSP option.");
+            }
+        }
+
+        test = new CachedIndependenceQueries(test);
+        edu.cmu.tetrad.search.FcitMarkov search = new edu.cmu.tetrad.search.FcitMarkov(test, score);
+
+        // BOSS
+        search.setUseDataOrder(parameters.getBoolean(Params.USE_DATA_ORDER));
+        search.setNumStarts(parameters.getInt(Params.NUM_STARTS));
+        search.setUseBes(parameters.getBoolean(Params.USE_BES));
+
+        // FCIT
+        search.setCompleteRuleSetUsed(parameters.getBoolean(Params.COMPLETE_RULE_SET_USED));
+        search.setDepth(parameters.getInt(Params.DEPTH));
+        search.setMaxDiscriminatingPathLength(parameters.getInt(Params.MAX_DISCRIMINATING_PATH_LENGTH));
+        search.setRbRadius(parameters.getInt(Params.RB_RADIUS));
+        search.setRecursiveDepth(parameters.getInt(Params.RECURSIVE_DEPTH));
+        search.setTimeout(parameters.getLong(Params.TEST_TIMEOUT));
+
+        search.setReplicatingGraph(parameters.getBoolean(Params.TIME_LAG_REPLICATING_GRAPH));
+
+        if (parameters.getInt(Params.FCIT_STARTS_WITH) == 1) {
+            search.setStartWith(edu.cmu.tetrad.search.FcitMarkov.START_WITH.BOSS);
+        } else if (parameters.getInt(Params.FCIT_STARTS_WITH) == 2) {
+            search.setStartWith(edu.cmu.tetrad.search.FcitMarkov.START_WITH.GRASP);
+        } else if (parameters.getInt(Params.FCIT_STARTS_WITH) == 3) {
+            search.setStartWith(edu.cmu.tetrad.search.FcitMarkov.START_WITH.SP);
+        } else if (parameters.getInt(Params.FCIT_STARTS_WITH) == 4) {
+            search.setStartWith(edu.cmu.tetrad.search.FcitMarkov.START_WITH.COMPLETE_GRAPH);
+        } else {
+            throw new IllegalArgumentException("Unknown start with option: " + parameters.getInt(Params.FCIT_STARTS_WITH));
+        }
+
+        // General
+        search.setVerbose(parameters.getBoolean(Params.VERBOSE));
+        search.setKnowledge(this.knowledge);
+
+        return search.search();
+    }
+
+    /**
+     * Retrieves a comparison graph by transforming a true directed graph into a partially directed graph (PAG).
+     *
+     * @param graph The true directed graph, if there is one.
+     * @return The comparison graph.
+     */
+    @Override
+    public Graph getComparisonGraph(Graph graph) {
+        return GraphTransforms.dagToPag(graph, false);
+    }
+
+    /**
+     * Returns a short, one-line description of this algorithm. The description is generated by concatenating the
+     * descriptions of the test and score objects associated with this algorithm.
+     *
+     * @return The description of this algorithm.
+     */
+    @Override
+    public String getDescription() {
+        return "FCIT-Markov (FCI Targeted Testing Markov) using "+ this.test.getDescription() + " and " + this.score.getDescription();
+    }
+
+    /**
+     * Retrieves the data type required by the search algorithm.
+     *
+     * @return The data type required by the search algorithm.
+     */
+    @Override
+    public DataType getDataType() {
+        return this.score.getDataType();
+    }
+
+    /**
+     * Retrieves the list of parameters used by the algorithm.
+     *
+     * @return The list of parameters used by the algorithm.
+     */
+    @Override
+    public List<String> getParameters() {
+        List<String> params = new ArrayList<>();
+
+        // BOSS
+        params.add(Params.USE_BES);
+        params.add(Params.USE_DATA_ORDER);
+        params.add(Params.NUM_STARTS);
+
+        // FCIT
+        params.add(Params.FCIT_STARTS_WITH);
+        params.add(Params.COMPLETE_RULE_SET_USED);
+        params.add(Params.DEPTH);
+        params.add(Params.MAX_DISCRIMINATING_PATH_LENGTH);
+        params.add(Params.RB_RADIUS);
+        params.add(Params.RECURSIVE_DEPTH);
+        params.add(Params.TEST_TIMEOUT);
+
+        // General
+        params.add(Params.TIME_LAG);
+        params.add(Params.TIME_LAG_REPLICATING_GRAPH);
+
+        params.add(Params.VERBOSE);
+
+        return params;
+    }
+
+    /**
+     * Retrieves the knowledge object associated with this method.
+     *
+     * @return The knowledge object.
+     */
+    @Override
+    public Knowledge getKnowledge() {
+        return this.knowledge;
+    }
+
+    /**
+     * Sets the knowledge object associated with this method.
+     *
+     * @param knowledge the knowledge object to be set
+     */
+    @Override
+    public void setKnowledge(Knowledge knowledge) {
+        this.knowledge = new Knowledge(knowledge);
+    }
+
+    /**
+     * Retrieves the ScoreWrapper object associated with this method.
+     *
+     * @return The ScoreWrapper object associated with this method.
+     */
+    @Override
+    public ScoreWrapper getScoreWrapper() {
+        return this.score;
+    }
+
+    /**
+     * Sets the score wrapper for the algorithm.
+     *
+     * @param score the score wrapper.
+     */
+    @Override
+    public void setScoreWrapper(ScoreWrapper score) {
+        this.score = score;
+    }
+
+    @Override
+    public IndependenceWrapper getIndependenceWrapper() {
+        return test;
+    }
+
+    @Override
+    public void setIndependenceWrapper(IndependenceWrapper independenceWrapper) {
+        this.test = independenceWrapper;
+    }
+}
+
